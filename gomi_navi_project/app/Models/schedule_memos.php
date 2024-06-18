8bf00a7a3b4c7852b28d67a48e3f04f335a40810
yumi <s_mog31@yahoo.co.jp>
2024-06-18 21:55:24 +0900

    add:schedule_memosテーブルの追加

---
+++ gomi_navi_project/app/Models/schedule_memos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class schedule_memos extends Model
+{
+    use HasFactory;
+}
