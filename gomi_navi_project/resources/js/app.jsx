06170c4b260f0429172518494766b41c61f4609e
yumi <s_mog31@yahoo.co.jp>
2024-06-18 20:43:09 +0900

    add:プロジェクト作成

---
+++ gomi_navi_project/resources/js/app.jsx
@@ -0,0 +1,13 @@
+import './bootstrap';
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, Laravel and React!</h1>
+        </div>
+    );
+};
+
+export default App;
+
