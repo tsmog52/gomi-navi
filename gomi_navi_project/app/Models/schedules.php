19b2ea5f030aee224865300675af9261b2c5f975
yumi <s_mog31@yahoo.co.jp>
2024-06-20 07:22:26 +0900

    rename:Model名の修正

--- gomi_navi_project/app/Models/schedules.php
+++ gomi_navi_project/app/Models/schedules.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class schedules extends Model
+class Schedules extends Model
 {
     use HasFactory;
 
--- gomi_navi_project/app/Models/sizes.php
+++ gomi_navi_project/app/Models/sizes.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class sizes extends Model
+class Sizes extends Model
 {
     use HasFactory;
 
