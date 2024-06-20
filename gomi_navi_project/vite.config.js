772a8956665e229176c8319a5f0883b904d075d9
yumi <s_mog31@yahoo.co.jp>
2024-06-21 07:12:16 +0900

    add:tailwindの追加

--- gomi_navi_project/vite.config.js
+++ gomi_navi_project/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.jsx',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
-        react(),
     ],
 });
