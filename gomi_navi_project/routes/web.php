45a89a6043dbe949b67022d367d5fa9a8d83a890
yumi <s_mog31@yahoo.co.jp>
2024-06-23 17:23:07 +0900

    feat:ルーティングの追加

--- gomi_navi_project/routes/web.php
+++ gomi_navi_project/routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/contact', 'contact');
 
+Route::view('/sorting_guide', 'sorting_guide');
+
+Route::view('/calender', 'calender');
+
+Route::view('/sorting_search', 'sorting_search');
+
+
+
 Route::get('/{any}', function () {
   return view('app');
 })->where('any', '.*');
