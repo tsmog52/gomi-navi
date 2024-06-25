afb6f7067eac563f37a3bb9f8d038b86ac958d3a
yumi <s_mog31@yahoo.co.jp>
2024-06-25 21:39:26 +0900

    clean:モーダルのコンポーネント化

---
+++ gomi_navi_project/resources/js/components/PickupModal.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { AiOutlineClose } from "react-icons/ai";
+
+const PickupModal = ({ onClose }) => {
+  return (
+    <>
+      <div
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      >
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+          <button onClick={onClose}>
+            <AiOutlineClose />
+          </button>
+          <div className='text-center'>
+            <h2
+              className="block text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-4"
+            >
+              今日のゴミ回収は
+            </h2>
+            <p>〇〇ごみです。</p>
+            <p>収集当日の朝8時までに出してください。<br />祝日も回収します。</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default PickupModal
