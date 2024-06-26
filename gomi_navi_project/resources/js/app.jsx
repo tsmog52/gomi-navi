bc09552ac0dac4022a7df36a8ccf78ca435bf055
yumi <s_mog31@yahoo.co.jp>
2024-06-26 20:32:23 +0900

    improve:コンポーネント化

--- gomi_navi_project/resources/js/app.jsx
+++ gomi_navi_project/resources/js/app.jsx
@@ -7,8 +7,7 @@ import Header from './components/Header';
 import useModal from './hooks/useModal';
 import SettingModal from './components/SettingModal';
 import PickupModal from './components/PickupModal';
-
-
+import Card from './components/Card';
 
 const App = () => {
   //モーダル1:今日のゴミ回収(分割代入)
@@ -21,56 +20,44 @@ const App = () => {
       <Header />
           <div className='flex justify-center items-center m-16 '>
             <main className="grid grid-cols-3 gap-16 items-center mt-10">
-              <div
-                className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg"
-              >
-                <button onClick={openModal1}>
-                  <FaTruck size={60} color={'#272f3f'} className="ml-4"/>
-                  <p className='pt-4'>今日のゴミ回収</p>
-                </button>
+              <Card
+                icon={FaTruck}
+                text='今日のゴミ回収'
+                onClick={openModal1}
+                size={60}
+              />
                 {isOpenModal1 &&  <PickupModal onClose={closeModal1}/>}
-              </div>
-              <div
-                className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg"
-              >
-              <a href='/sorting_search'>
-                <FaSearch size={50} color={'#272f3f'} className="ml-4"/>
-                <p className='pt-4'>ゴミの分別検索</p>
-              </a>
-            </div>
-            <div
-              className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg"
-            >
-              <a href='calender'>
-                <FaCalendarAlt size={50} color={'#272f3f'} className="ml-4"/>
-                <p className='pt-4'>カレンダー</p>
-              </a>
-            </div>
-            <div
-              className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg"
-            >
-              <a href='/categories'>
-                <AiFillDelete size={60} color={'#272f3f'} className="ml-4"/>
-                <p className='pt-4'>ゴミの捨て方</p>
-              </a>
-            </div>
-            <div
-              className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg"
-            >
-              <a href='/contact'>
-                <FaPhone size={50} color={'#272f3f'} className="ml-4"/>
-                <p className='pt-4'>お問い合わせ</p>
-              </a>
-            </div>
-            <div
-              className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg"
-            >
-              <button onClick={openModal2}>
-                <IoSettingsSharp size={60} color={'#272f3f'} />
-                <p className='pt-4'>設定</p>
-              </button>
+              <Card
+                icon={FaSearch}
+                link={'/sorting_search'}
+                text='ゴミの分別検索'
+                size={50}
+              />
+              <Card
+                icon={FaCalendarAlt}
+                link={'/calender'}
+                text='カレンダー'
+                size={50}
+              />
+              <Card
+                icon={AiFillDelete}
+                link={'/index'}
+                text='ゴミの捨て方'
+                size={60}
+              />
+              <Card
+                icon={FaPhone}
+                link={'/contact'}
+                text='お問い合わせ'
+                size={50}
+              />
+              <Card
+                icon={IoSettingsSharp}
+                text='設定'
+                onClick={openModal2}
+                size={60}
+              />
               {isOpenModal2 &&  <SettingModal onClose={closeModal2}/>}
-            </div>
           </main>
         </div>
     </>
