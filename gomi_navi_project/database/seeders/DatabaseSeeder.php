96ce0b840846317578eab6936f33d9fbe35bcdaf
yumi <s_mog31@yahoo.co.jp>
2024-06-26 16:15:36 +0900

    feat:itemsテーブルの初期値追加

--- gomi_navi_project/database/seeders/DatabaseSeeder.php
+++ gomi_navi_project/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RegionsTableSeeder::class,
             CategoriesTableSeeder::class,
-            SortingGuidesTableSeeder::class
+            SortingGuidesTableSeeder::class,
+            ItemsSeeder::class
         ]);
     }
 }
