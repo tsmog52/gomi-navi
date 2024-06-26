1aab30ed3c5814fef32d72a19c923a7bf1615482
yumi <s_mog31@yahoo.co.jp>
2024-06-26 16:16:07 +0900

    rename:ファイル名の修正

--- gomi_navi_project/resources/views/categories.blade.php
+++ gomi_navi_project/resources/views/categories.blade.php
@@ -4,9 +4,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
-  @vite('resources/js/sorting_guide.jsx')
+  @vite('resources/js/components/Category.jsx')
 </head>
 <body>
-  <div id="sorting_guide"></div>
+  <div id="category"></div>
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </body>
 </html>
