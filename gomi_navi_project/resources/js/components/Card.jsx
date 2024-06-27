48e71d053cb04cb34363b4e3f5d83a63f92d3b5f
yumi <s_mog31@yahoo.co.jp>
2024-06-27 09:21:50 +0900

    clean:divをbuttonへ変更

--- gomi_navi_project/resources/js/components/Card.jsx
+++ gomi_navi_project/resources/js/components/Card.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Card = ({ text, icon:Icon, link, onClick, size }) => {
   return (
     <>
-      <div
+      <button
         className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg" onClick={onClick}
       >
         <a href={link}>
           <Icon size={size} color={'#272f3f'} className="ml-4"/>
           <p className='pt-4'>{text}</p>
         </a>
-      </div>
+      </button>
     </>
   )
 }
