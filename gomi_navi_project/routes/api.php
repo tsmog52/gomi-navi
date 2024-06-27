4442df7c4ba09f8f6db72bc7b12870231379e823
yumi <s_mog31@yahoo.co.jp>
2024-06-27 12:11:34 +0900

    feat:apiのインストール

--- gomi_navi_project/routes/api.php
+++ gomi_navi_project/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/categories', [CategoryController::class, 'index']);
+
