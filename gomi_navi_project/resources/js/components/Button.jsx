c4780626a7438302e86afe46b1bdbb1b8911f901
yumi <s_mog31@yahoo.co.jp>
2024-06-27 09:23:11 +0900

    feat:buttonのコンポーネント化

---
+++ gomi_navi_project/resources/js/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({ text }) => {
+  return (
+    <>
+      <buton
+        className='items-center text-color-white bg-gray-500 w-30 h-15 p-3 rounded-lg text-white ml-32'
+      >
+        {text}
+      </buton>
+    </>
+  )
+}
+
+export default Button
