eb944f16857c5b6afb96cb5aafd77e346127504a
yumi <s_mog31@yahoo.co.jp>
2024-06-28 07:00:35 +0900

    feat:ルーティングの追加

--- gomi_navi_project/routes/api.php
+++ gomi_navi_project/routes/api.php
@@ -3,10 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SchedulePatternsController;
+use App\Models\SortingGuide;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/sorting_guides', [SortingGuide::class, 'index']);
+
+Route::get('/schedule_patterns', [SchedulePatternsController::class, 'index']);
+
 
