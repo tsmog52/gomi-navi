456da22fc6b6a2bd0af95e7e4cc4e507915486af
yumi <s_mog31@yahoo.co.jp>
2024-06-29 17:46:33 +0900

    improve:ルーティングの変更

--- gomi_navi_project/routes/web.php
+++ gomi_navi_project/routes/web.php
@@ -8,9 +8,13 @@ Route::view('/contact', 'contact');
 
 Route::view('/calender', 'calender');
 
-Route::view('/sorting_search', 'sorting_search');
+Route::view('/item', 'item');
 
 Route::view('/index', 'categories');
+
+Route::view('/schedule_patterns', 'sorting_guides');
+
+
 
 
 
