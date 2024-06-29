833b12c763d7513b7b3512c0a8d545592304e57c
yumi <s_mog31@yahoo.co.jp>
2024-06-29 12:35:49 +0900

    feat:categoryとのリレーション追加

--- gomi_navi_project/app/Models/SortingGuide.php
+++ gomi_navi_project/app/Models/SortingGuide.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class SortingGuide extends Model
 {
@@ -13,4 +14,7 @@ class SortingGuide extends Model
         'category_id',
         'instruction'
     ];
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
