6fa202f1a2e433f5533081455f67a2d839d9af95
yumi <s_mog31@yahoo.co.jp>
2024-06-29 17:45:46 +0900

    feat:itemsルーティングの追加

--- gomi_navi_project/routes/api.php
+++ gomi_navi_project/routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemsController;
 use App\Http\Controllers\SchedulePatternsController;
+use App\Http\Controllers\SortingGuidesController;
 use App\Models\SortingGuide;
 
 Route::get('/user', function (Request $request) {
@@ -11,9 +13,12 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/categories', [CategoryController::class, 'index']);
-
-Route::get('/sorting_guides', [SortingGuide::class, 'index']);
-
+//スケジュールパターンテーブルから取得
 Route::get('/schedule_patterns', [SchedulePatternsController::class, 'index']);
+
+Route::get('/instruction', [SortingGuidesController::class, 'index']);
+
+Route::get('/items', [ItemsController::class, 'index']);
+
 
 
