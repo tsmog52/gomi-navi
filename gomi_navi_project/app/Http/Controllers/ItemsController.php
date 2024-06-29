3c8887011d459647c8e01fb86dc2c83dfca13540
yumi <s_mog31@yahoo.co.jp>
2024-06-29 17:45:02 +0900

    feat:コントローラーの作成

---
+++ gomi_navi_project/app/Http/Controllers/ItemsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Response;
+
+class ItemsController extends Controller
+{
+    public function index() {
+        $items = Item::with('category')->get();
+        $response = $items->map(function ($item) {
+            return [
+                //itemsテーブルからitem_nameカラムを取得
+                'item_name' => $item->item_name,
+                //itemsテーブルからcategories(FK)、category_nameカラムを取得
+                'category_name' => $item->category->category_name,
+                //itemsテーブルからitem_memoカラムを取得
+                'item_memo' => $item->item_memo
+            ];
+        });
+        return response()->json($response, Response::HTTP_OK);
+    }
+}
