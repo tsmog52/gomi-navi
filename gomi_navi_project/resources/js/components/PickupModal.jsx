ecdc5d5ad843ad0c15dafa3c305035cc6ab69f89
yumi <s_mog31@yahoo.co.jp>
2024-06-29 13:10:44 +0900

    clean:buttonのコンポーネント化

--- gomi_navi_project/resources/js/components/PickupModal.jsx
+++ gomi_navi_project/resources/js/components/PickupModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AiOutlineClose } from "react-icons/ai";
+import CloseButton from './Buttons/CloseButton';
 
 const PickupModal = ({ onClose }) => {
   return (
@@ -8,9 +9,7 @@ const PickupModal = ({ onClose }) => {
         className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
       >
         <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-          <button onClick={onClose}>
-            <AiOutlineClose />
-          </button>
+          <CloseButton onClose={onClose}/>
           <div className='text-center'>
             <h2
               className="block text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-4"
