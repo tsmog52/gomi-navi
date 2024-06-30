c870013dc43aeed671c84f1332f4cbc70f968632
yumi <s_mog31@yahoo.co.jp>
2024-06-30 19:02:37 +0900

    clean:ファイルの相対パス変更

--- gomi_navi_project/resources/views/categories.blade.php
+++ gomi_navi_project/resources/views/categories.blade.php
@@ -4,10 +4,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
-  @vite('resources/js/components/Category.jsx')
+  @vite('resources/js/Category.jsx')
 </head>
 <body>
   <div id="category"></div>
-  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </body>
 </html>
