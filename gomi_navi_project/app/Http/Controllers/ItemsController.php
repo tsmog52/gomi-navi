b0ee642c5791eaa30da4c3b0afa4ecae8c5cc310
yumi <s_mog31@yahoo.co.jp>
2024-07-01 17:51:12 +0900

    feat:pagenateの追加

--- gomi_navi_project/app/Http/Controllers/ItemsController.php
+++ gomi_navi_project/app/Http/Controllers/ItemsController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Response;
 class ItemsController extends Controller
 {
     public function index() {
-        $items = Item::with('category')->get();
+        $items = Item::paginate(8);
         $response = $items->map(function ($item) {
             return [
                 //itemsテーブルからitem_nameカラムを取得
@@ -17,8 +17,8 @@ class ItemsController extends Controller
                 'category_name' => $item->category->category_name,
                 //itemsテーブルからitem_memoカラムを取得
                 'item_memo' => $item->item_memo
-            ];
-        });
-        return response()->json($response, Response::HTTP_OK);
-    }
+                ];
+                });
+                return response()->json($response, Response::HTTP_OK);
+            }
 }
