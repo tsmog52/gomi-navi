d486e46d9fca5158b824db6f173fb980879f39eb
yumi <s_mog31@yahoo.co.jp>
2024-07-01 17:46:32 +0900

    feat:カレンダーページの作成

--- gomi_navi_project/resources/js/calendar.jsx
+++ gomi_navi_project/resources/js/calendar.jsx
@@ -1,63 +1,69 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';
 
-
 const Calendar = () => {
-  const [schedules, setSchedules] = useState([]);
+  const [calendars, setCalendars] = useState([]);
+  //en→jaへ変換
+const daysOfWeek = {
+  'MO': '月', 'TU': '火', 'WE': '水', 'TH': '木', 'FR': '金', 'SA': '土','SU': '日'
+};
+
+//曜日の順序を指定
+const daysOrder = {
+  'MO': 1, 'TU': 2, 'WE': 3, 'TH': 4, 'FR': 5, 'SA': 6,'SU': 7
+};
+
+const sortCalendars = (calendars) => {
+  return calendars.sort((a, b) => {
+    //プロパティ値を(',')で分割し配列に入れる
+    const aDays = a.days_of_week.split(',').map(day => daysOrder[day.trim()]);
+    const bDays = b.days_of_week.split(',').map(day => daysOrder[day.trim()]);
+    //最小値の取得→昇順
+    return Math.min(...aDays) - Math.min(...bDays);
+  });
+};
 
   useEffect(() => {
-    const getSchedulesData = async () => {
+    const getCalendarData = async () => {
       try {
-        const response = await axios.get('/api/schedule_patterns');
-        setSchedules(response.data.schedulePatterns);
+        const response = await axios.get('/api/calendar');
+        const sortedData = sortCalendars(response.data);
+        setCalendars(sortedData);
       } catch (error) {
-        if (error.response) {
-          console.error('サーバーエラー:', error.response.data);
-        } else if (error.request) {
-          console.error('レスポンスがありませんでした:', error.request);
-        } else {
-          console.error('リクエストに問題がありました:', error.message);
-        }
         console.error('通信に失敗しました:', error);
       }
     };
-    getSchedulesData();
+    getCalendarData();
   }, []);
 
+const renderCalendarsForDay = (day) => {
+  return calendars
+  .filter(calendar => calendar.days_of_week.split(',').includes(day))
+  //オブジェクトからcategory_nameを取得
+  .map((calendar, index) => (
+      <span key={index}>{calendar.category_name}</span>
+  ));
+};
+
   return (
-    <div>
-      <p>これはカレンダーです。</p>
-      <p>月</p>
-      <div className='border-b-2'></div>
-      <p>火</p>
-      <div className='border-b-2'></div>
-      <p>水</p>
-      <div className='border-b-2'></div>
-      <p>木</p>
-      <div className='border-b-2'></div>
-      <p>金</p>
-      <div className='border-b-2'></div>
-      <p>土</p>
-      <div className='border-b-2'></div>
-      <p>日</p>
-      <div className='border-b-2'></div>
-        <ul>
-          {schedules.map(schedule => (
-              <li
-                className='text-xl p-4 flex'
-              >
-                {schedule.days_of_week}
-              </li>
+    <>
+      <di>
+        {/* daysOrderのday:key label:value */}
+          {Object.entries(daysOfWeek).map(([day, label]) => (
+            <div key={day} className='flex'>
+              <p className='text-3xl items-center p-4'>{label}</p>
+              <p className='text-2xl'>{renderCalendarsForDay(day)}</p>
+            </div>
           ))}
-        </ul>
-    </div>
-  )
-}
+          <textarea className='border-2'></textarea>
+      </di>
+    </>
+  );
+};
 
 export default Calendar;
 
 const container = document.getElementById('calendar');
 const root = createRoot(container);
-root.render(< Calendar />);
-
+root.render(<Calendar />);
