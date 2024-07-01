72c4a328659bdba0f896b17f22d93bdd522cb04c
yumi <s_mog31@yahoo.co.jp>
2024-07-01 17:45:16 +0900

    feat:リレーションの追加

--- gomi_navi_project/app/Models/Schedule.php
+++ gomi_navi_project/app/Models/Schedule.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use App\Models\SchedulePattern;
 
 class Schedule extends Model
 {
@@ -16,8 +17,12 @@ class Schedule extends Model
         'schedule_pattern_id'
     ];
 
-    public function categories() {
+    public function category() {
         return $this->belongsTo(Category::class);
+    }
+
+    public function schedulePattern() {
+        return $this->belongsTo(SchedulePattern::class);
     }
 
 }
