70b5b653e2e18d68dca49307c52a39a8f77acd6a
yumi <s_mog31@yahoo.co.jp>
2024-07-01 17:49:10 +0900

    clean:コード整形

--- gomi_navi_project/resources/views/calendar.blade.php
+++ gomi_navi_project/resources/views/calendar.blade.php
@@ -4,9 +4,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
-  @vite('resources/js/calender.jsx')
+  @vite('resources/js/calendar.jsx')
 </head>
 <body>
-  <div id="calender"></div>
+  <div id="calendar"></div>
 </body>
 </html>
