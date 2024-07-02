463d94245e62dde9c9143b8aef3bd557b67a2631
yumi <s_mog31@yahoo.co.jp>
2024-07-02 20:25:05 +0900

    update:リンクの修正

--- gomi_navi_project/resources/js/components/Button/SocialMediaButton.jsx
+++ gomi_navi_project/resources/js/components/Button/SocialMediaButton.jsx
@@ -5,7 +5,7 @@ const SocialMediaButton = ({ text }) => {
     <>
       <a
         className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-800 dark:text-white dark:hover:bg-neutral-800 mb-4"
-        href="#"
+        href="/auth/redirect"
       >
         <svg className="w-4 h-auto" width="46" height="47" viewBox="0 0 46 47" fill="none">
           <path
