541ee5c67d3f71df79d6bc036ae66c1effe0372d
yumi <s_mog31@yahoo.co.jp>
2024-07-02 20:26:49 +0900

    update:radioボタンの大きさ変更

--- gomi_navi_project/resources/js/components/Modal/SettingModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/SettingModal.jsx
@@ -3,7 +3,7 @@ import { Field, Label, Radio, RadioGroup } from '@headlessui/react';
 import SocialMediaButton from '../Button/SocialMediaButton';
 import CloseButton from '../Button/CloseButton';
 
-const regions = ['川崎区', '幸区', '中原区', '高津区', '宮前区', '多摩区', '麻生区'];
+const regions = ['川崎区', '中原区', '高津区', '宮前区', '多摩区', '麻生区', '幸区 '];
 
 const SettingModal = ({ onClose }) => {
 
@@ -21,18 +21,20 @@ const [selected, setSelected] = useState(regions[0])
         </div>
         <div className="border-b-2"></div>
         <p className='pt-4 text-center font-bold text-xl'>地域を設定する</p>
-        <RadioGroup value={selected} onChange={setSelected} aria-label="Server size">
-          {regions.map((region) => (
-          <Field key={region} className="flex items-center justify-center gap-2 pb-2 text-base">
-          <Radio
-            value={region}
-            className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
-          >
-            <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
-          </Radio>
-          <Label>{region}</Label>
-          </Field>
-        ))}
+        <RadioGroup value={selected} onChange={setSelected}>
+          <div className='grid grid-cols-2 p-2'>
+            {regions.map((region) => (
+            <Field key={region} className="w-20 flex items-center justify-center gap-2 pb-2 text-base">
+            <Radio
+              value={region}
+              className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-400"
+            >
+              <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
+            </Radio>
+            <Label>{region}</Label>
+            </Field>
+            ))}
+          </div>
           </RadioGroup>
             <p className='text-center font-bold text-xl p-4'>外部カレンダー連携</p>
             <SocialMediaButton text='Googleと連携する' />
