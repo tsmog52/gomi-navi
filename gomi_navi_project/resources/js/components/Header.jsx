289fc1518c1661678a9fe861ab77ddad18ab3425
yumi <s_mog31@yahoo.co.jp>
2024-07-03 21:32:02 +0900

    feat:stateの追加

--- gomi_navi_project/resources/js/components/Header.jsx
+++ gomi_navi_project/resources/js/components/Header.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '/resources/css/app.css';
 import useModal from '../hooks/useModal';
 import Modal from './Modal/Modal';
+import AddMemoButton from './Button/AddMemoButton';
+import LogoutButton from './Button/LogoutButton';
 
 const Header = () => {
 //モーダル1:ログイン(分割代入)
   const {isOpen: isOpenModal1, open: openModal1, close: closeModal1} = useModal();
-  //モーダル2:会員登録(分割代入)
+//モーダル2:会員登録(分割代入)
   const {isOpen: isOpenModal2, open: openModal2, close: closeModal2} = useModal();
+//ricoilで管理したい
+  const [isVisible, setIsVisible] = useState(true);
 
   return (
     <>
       <header className='bg-gray-200 p-4'>
         <nav>
           <ul className='flex space-x-4 justify-end'>
+            <LogoutButton />
+            <li>
+              {isVisible && <AddMemoButton />}
+            </li>
             <li>
               <button onClick={openModal1}>ログイン</button>
               {isOpenModal1 && <Modal onClose={closeModal1} title='ログイン' text='Googleでログイン' linkText='会員登録がまだの方はこちら' />}
