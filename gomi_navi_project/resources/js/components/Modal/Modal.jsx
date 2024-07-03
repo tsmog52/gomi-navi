2c24144eae9ca17d2c67cc9982b3256a3b184ae7
yumi <s_mog31@yahoo.co.jp>
2024-07-03 20:26:57 +0900

    clean:コード整形

--- gomi_navi_project/resources/js/components/Modal/Modal.jsx
+++ gomi_navi_project/resources/js/components/Modal/Modal.jsx
@@ -12,7 +12,7 @@ const Modal = ({title, linkText, onClose, text }) => {
             <h2 className="block text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-4">
               {title}
             </h2>
-            <SocialMediaButton text={text} />
+            <SocialMediaButton text={text}/>
               <p className="mb-4">
                 <a className="text-blue-600 underline hover:text-blue-400" href="#">
                   {linkText}
