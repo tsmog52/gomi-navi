6a703ca40a9a406f6f2137939e7ce554439f2c5c
yumi <s_mog31@yahoo.co.jp>
2024-07-04 06:55:25 +0900

    update:インポートファイルの変更

--- gomi_navi_project/resources/views/contact.blade.php
+++ gomi_navi_project/resources/views/contact.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
-  @vite('resources/js/contact.jsx')
+  @vite('resources/js/Contact.jsx')
 </head>
 <body>
   <div id="contact"></div>
