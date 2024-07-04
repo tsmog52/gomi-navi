c5ca35b03b4ecf3a9617a83b9e9535ed9879656e
yumi <s_mog31@yahoo.co.jp>
2024-07-05 07:07:42 +0900

    feat:inputのコンポーネント化

---
+++ gomi_navi_project/resources/js/components/InputField.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+
+const InputField = ({  value, onChange, placeholder, onClick }) => {
+  return (
+    <>
+      <div className=' relative w-96 m-5 ml-12'>
+        <input
+          type='text'
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+          className='p-1 border-2 rounded  pr-14 w-full'
+        />
+        <button
+          onClick={onClick}
+          className='absolute top-1/2 right-0 transform -translate-y-1/2 p-2 hover:bg-gray-200 border-l-2 rounded-r'
+        >
+          <IoSearch size={20} className='text-gray-400' />
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default InputField
