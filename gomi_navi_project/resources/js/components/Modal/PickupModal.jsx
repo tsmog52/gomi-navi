81e4b3bd955d5dc199c99e69141d35aa676a1683
yumi <s_mog31@yahoo.co.jp>
2024-07-06 15:43:38 +0900

    feat:今日のゴミ回収機能追加

--- gomi_navi_project/resources/js/components/Modal/PickupModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/PickupModal.jsx
@@ -3,6 +3,21 @@ import { AiOutlineClose } from "react-icons/ai";
 import CloseButton from '../Button/CloseButton';
 
 const PickupModal = ({ onClose }) => {
+
+  //Q. DBで管理したほうかいいのか
+  const date = new Date();
+  const day = date.getDay();
+  const daysOfWeek = {
+    0: 'なし',
+    1: 'ミックスペーパー',
+    2: 'プラスチック製容器',
+    3: '普通ゴミ',
+    4: 'なし',
+    5: '空き缶・ペットボトル、空きビン、使用済み乾電池、粗大ゴミ(有料)、小物金属(第1・3のみ)',
+    6: '普通ゴミ'
+  };
+  const dayOfWeek = daysOfWeek[day];
+
   return (
     <>
       <div
@@ -11,13 +26,18 @@ const PickupModal = ({ onClose }) => {
         <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
           <CloseButton onClose={onClose}/>
           <div className='text-center'>
-            <h2
-              className="block text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-4"
+            <p
+              className='text-gray-800 dark:text-neutral-200 pb-4'
             >
-              今日のゴミ回収は
-            </h2>
-            <p>〇〇ごみです。</p>
-            <p>収集当日の朝8時までに出してください。<br />祝日も回収します。</p>
+              今日の回収は
+              <span
+              className='text-2xl font-bold text-gray-600'
+              >
+                {dayOfWeek}
+              </span>
+              です。
+            </p>
+            <p className='pb-2'>収集当日の朝8時までに出してください。<br /><span className='text-red-500'>祝日も回収します。</span></p>
           </div>
         </div>
       </div>
