99d96b8cc3169fca8e292ed9371f7ad04fa2a4c0
yumi <s_mog31@yahoo.co.jp>
2024-07-06 15:46:02 +0900

    feat:コントローラーの追加・修正

--- gomi_navi_project/routes/api.php
+++ gomi_navi_project/routes/api.php
@@ -3,10 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ItemsController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\SchedulePatternsController;
 use App\Http\Controllers\SortingGuidesController;
 use App\Http\Controllers\SchedulesController;
+use App\Models\ScheduleMemo;
+use App\Models\Item;
 use App\Models\SortingGuide;
 
 
@@ -16,9 +18,18 @@ Route::get('/schedule_patterns', [SchedulePatternsController::class, 'index']);
 
 Route::get('/instruction', [SortingGuidesController::class, 'index']);
 
-Route::get('/items', [ItemsController::class, 'index']);
+Route::get('/items', [ItemController::class, 'index']);
 
 Route::get('/calendar', [SchedulesController::class, 'index']);
+
+Route::get('/item', function () {
+  return Item::paginate();
+});
+
+// フロントで'/api/example'のURLが叩かれたらExampleControllerのsave関数を呼び出す
+Route::group(['middleware' => 'api'], function () {
+  Route::post('/schedule_memo', [ScheduleMemo::class, 'save']);
+});
 
 
 
