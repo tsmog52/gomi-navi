6ec307e284bbdbb4f6ce21a7e13cc9d9008ed90a
yumi <s_mog31@yahoo.co.jp>
2024-07-07 14:59:08 +0900

    feat:propsの追加

--- gomi_navi_project/resources/js/components/Modal/Modal.jsx
+++ gomi_navi_project/resources/js/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SocialMediaButton from '../Button/SocialMediaButton';
 import CloseButton  from '../Button/CloseButton';
 
-const Modal = ({title, linkText, onClose, text }) => {
+const Modal = ({title, linkText, onClose, google, line }) => {
   return (
     <>
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
@@ -12,7 +12,8 @@ const Modal = ({title, linkText, onClose, text }) => {
             <h2 className="block text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-4">
               {title}
             </h2>
-            <SocialMediaButton text={text}/>
+            <SocialMediaButton text={google} socialLink={"/auth/redirect"}/>
+            <SocialMediaButton text={line} socialLink={"/auth/line/redirect"}/>
               <p className="mb-4">
                 <a className="text-blue-600 underline hover:text-blue-400" href="#">
                   {linkText}
