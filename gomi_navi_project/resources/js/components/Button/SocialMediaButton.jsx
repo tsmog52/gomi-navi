1d10e2bd35591fee9c3ae81cecb2bfd1cea92a00
yumi <s_mog31@yahoo.co.jp>
2024-07-07 14:59:21 +0900

    feat:propsの追加

--- gomi_navi_project/resources/js/components/Button/SocialMediaButton.jsx
+++ gomi_navi_project/resources/js/components/Button/SocialMediaButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const SocialMediaButton = ({ text }) => {
+const SocialMediaButton = ({ text, socialLink }) => {
   return (
     <>
       <a
         className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-800 dark:text-white dark:hover:bg-neutral-800 mb-4"
-        href="/auth/redirect"
+        href={socialLink}
       >
         <svg className="w-4 h-auto" width="46" height="47" viewBox="0 0 46 47" fill="none">
           <path
