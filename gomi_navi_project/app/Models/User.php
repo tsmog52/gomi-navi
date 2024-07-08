5f1a72d54f9fe54efacd2861aedd5ae5657d4bd8
yumi <s_mog31@yahoo.co.jp>
2024-07-08 16:48:10 +0900

    feat:リレーションの追加

--- gomi_navi_project/app/Models/User.php
+++ gomi_navi_project/app/Models/User.php
@@ -2,14 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as AuthenticatableUser;
 
-class User extends Model
+class User extends  AuthenticatableUser implements AuthenticatableContract
 {
     use HasFactory;
-    protected $fillable = [
-        'provider',
-        'provider_id'
-    ];
+
+    public function socialPersonalAccessTokens()
+    {
+        return $this->hasMany(SocialPersonalAccessToken::class);
+    }
+
+    public function socialAccounts()
+    {
+        return $this->hasMany(SocialAccount::class);
+    }
+
+    public function scheduleMemos()
+    {
+        return $this->hasMany(ScheduleMemo::class);
+    }
+
+
+    // 認証のために必要なメソッド
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
 }
