218e6398ae4b942c925ec46ae51b01f2a41650e8
yumi <s_mog31@yahoo.co.jp>
2024-07-08 13:33:19 +0900

    feat:footerの追加

---
+++ gomi_navi_project/resources/js/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div className='bg-gray-200 p-4'>
+        <p className='flex items-center justify-center text-sm'>
+          <span className='pr-1'>&copy;</span>gomi-navi
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
