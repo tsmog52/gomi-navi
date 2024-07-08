a2e841869d288bfeb64601d0d3c08d43ed403e26
yumi <s_mog31@yahoo.co.jp>
2024-07-08 21:32:22 +0900

    feat:コントローラーへの分離

--- gomi_navi_project/routes/web.php
+++ gomi_navi_project/routes/web.php
@@ -2,12 +2,8 @@
 
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
-use APP\Http\Controllers\ItemController;
-use App\Models\PersonalAccessToken;
+use App\Http\Controllers\Auth\GoogleAuthController;
+use App\Http\Controllers\ItemController;
 
 // お問い合わせ
 Route::view('/contact', 'contact');
@@ -22,49 +18,10 @@ Route::view('/', 'app')->name('home');
 
 Route::get('/items', [ItemController::class, 'index'])->name('item');
 
-// OAuth認証するためのURLにリダイレクトする(カレンダー)
-Route::get('auth/redirect', function() {
-    return Socialite::driver('google')
-    ->scopes(['https://www.googleapis.com/auth/calendar.events'])
-    ->with(['access_type' => 'offline'])
-    ->redirect();
-});
-
-// ユーザー認証
-Route::get('/auth/callback', function() {
-    $social_user = Socialite::driver('google')->user();
-
-  // 既存のユーザーの検索
-    $user = User::where('provider_id', $social_user->getId())
-        ->where('provider', 'google')
-        ->first();
-
-    if (!$user) {
-      // ユーザーが存在しない場合、新規作成
-        $user = User::create([
-            'provider_id' => $social_user->getId(),
-            'provider' => 'google',
-        ]);
-    }
-
-  // PersonalAccessTokenの検索または新規作成
-    $personalAccessToken = PersonalAccessToken::updateOrCreate(
-        [
-            'user_id' => $user->id,
-        ],
-        [
-            'token' => $social_user->token,
-            'refresh_token' => $social_user->refreshToken,
-            'expires_at' => Carbon::now()->addSeconds($social_user->expiresIn),
-        ]
-    );
-
-  // ユーザーログイン
-    Auth::login($user);
-
-    return redirect('/');
+Route::middleware(['web'])->group(function () {
+    Route::get('auth/redirect', [GoogleAuthController::class, 'redirect']);
+    Route::get('/auth/callback', [GoogleAuthController::class, 'callback']);
 });
 
 //ログアウト処理
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
