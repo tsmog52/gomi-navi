1a7087ab6708971665afd4b6bbbcd7cf6c5e0cf2
yumi <s_mog31@yahoo.co.jp>
2024-07-09 21:50:06 +0900

    feat:メモの保存機能追加

--- gomi_navi_project/app/Http/Controllers/ScheduleMemoController.php
+++ gomi_navi_project/app/Http/Controllers/ScheduleMemoController.php
@@ -3,19 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\ScheduleMemo;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
 class ScheduleMemoController extends Controller
 {
-    public function save(Request $request)
+    public function store(Request $request)
     {
-        $request->validate(
-            ['note' => 'require|string']
-        );
-
-
-        ScheduleMemo::create([
-            'note' => $request->note
+        $inputs = $request->validate([
+                'note' => 'required|string',
+                'schedule_id' => 'required|exists:schedules,id',
         ]);
+
+        $scheduleMemo = new ScheduleMemo();
+        $scheduleMemo->note = $inputs['note'];
+        $scheduleMemo->schedule_id = $inputs('schedule_id');
+        $scheduleMemo->user_id = Auth::id();
+        $scheduleMemo->save();
+
+        return response()->json([
+            'scheduleMemo' => $scheduleMemo
+        ], Response::HTTP_OK);
     }
 }
