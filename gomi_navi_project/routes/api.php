c557954069b7f9db284496cdb043f35bebc6bd7e
yumi <s_mog31@yahoo.co.jp>
2024-07-09 21:41:07 +0900

    feat:tokenの発行処理の追加

--- gomi_navi_project/routes/api.php
+++ gomi_navi_project/routes/api.php
@@ -1,35 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\SchedulePatternsController;
 use App\Http\Controllers\SortingGuidesController;
 use App\Http\Controllers\SchedulesController;
-use App\Models\ScheduleMemo;
 use App\Models\Item;
-use App\Models\SortingGuide;
 
-
+//認証不要なルート
 Route::get('/categories', [CategoryController::class, 'index']);
-
 Route::get('/schedule_patterns', [SchedulePatternsController::class, 'index']);
-
 Route::get('/instruction', [SortingGuidesController::class, 'index']);
-
 Route::get('/items', [ItemController::class, 'index']);
-
 Route::get('/calendar', [SchedulesController::class, 'index']);
-
 Route::get('/item', function () {
   return Item::paginate();
 });
 
-// フロントで'/api/example'のURLが叩かれたらExampleControllerのsave関数を呼び出す
-Route::group(['middleware' => 'api'], function () {
-  Route::post('/schedule_memo', [ScheduleMemo::class, 'save']);
-});
+// API トークンを発行するエンドポイント(ユーザー認証済みかを確認)
+Route::middleware('auth:sanctum')->post('/tokens/create', function (Request $request) {
+  // 認証されたユーザーのみがアクセスできる
+  $user = $request->user();
+  $token = $user->createToken('auth_token')->plainTextToken;
 
+  return response()->json(['token' => $token]);
+});
 
 
