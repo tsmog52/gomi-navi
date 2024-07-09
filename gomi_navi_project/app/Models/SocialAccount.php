2bed4bf672a407cb1bea272529e5aced9a52a7df
yumi <s_mog31@yahoo.co.jp>
2024-07-09 21:48:25 +0900

    feat:fillableの追加

--- gomi_navi_project/app/Models/SocialAccount.php
+++ gomi_navi_project/app/Models/SocialAccount.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
 
 class SocialAccount extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'provider', 'provider_id'
+    ];
 
     public function user()
     {
