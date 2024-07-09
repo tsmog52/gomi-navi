a37745324e89a054658e4c4175b33d638ae7b09a
yumi <s_mog31@yahoo.co.jp>
2024-07-09 21:42:39 +0900

    feat:SocialAuthControllerの追加

--- gomi_navi_project/routes/web.php
+++ gomi_navi_project/routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\GoogleAuthController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\SocialAuthController;
 
 // お問い合わせ
 Route::view('/contact', 'contact');
@@ -18,10 +18,14 @@ Route::view('/', 'app')->name('home');
 
 Route::get('/items', [ItemController::class, 'index'])->name('item');
 
-Route::middleware(['web'])->group(function () {
-    Route::get('auth/redirect', [GoogleAuthController::class, 'redirect']);
-    Route::get('/auth/callback', [GoogleAuthController::class, 'callback']);
-});
+// GoogleAuthのルート
+Route::get('login/google', [SocialAuthController::class, 'redirectToGoogle']);
+Route::get('login/google/callback', [SocialAuthController::class, 'handleGoogleCallback']);
+
+// LINEAuthのルート
+Route::get('login/line', [SocialAuthController::class, 'redirectToLine']);
+Route::get('login/line/callback', [SocialAuthController::class, 'handleLineCallback']);
+
 
 //ログアウト処理
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
