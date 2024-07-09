20ea813f6911fa2c26eff256181012817a896022
yumi <s_mog31@yahoo.co.jp>
2024-07-09 21:43:45 +0900

    clean:lineの追加

--- gomi_navi_project/config/services.php
+++ gomi_navi_project/config/services.php
@@ -42,8 +42,8 @@ return [
     ],
 
     'line' => [
-    'client_id' => env('LINE_CLIENT_ID'),
-    'client_secret' => env('LINE_CLIENT_SECRET'),
-    'redirect' => env('LINE_REDIRECT_URI')
+        'client_id' => env('LINE_CLIENT_ID'),
+        'client_secret' => env('LINE_CLIENT_SECRET'),
+        'redirect' => env('LINE_REDIRECT_URI')
     ],
 ];
