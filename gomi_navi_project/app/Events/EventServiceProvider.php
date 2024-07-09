722b752f2bf74e1ebcfa3c68a9ea794d2e8d18cf
yumi <s_mog31@yahoo.co.jp>
2024-07-09 21:47:14 +0900

    feat:LINEイベントの追加

---
+++ gomi_navi_project/app/Events/EventServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+
+        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+            $event->extendSocialite('line', \SocialiteProviders\Line\Provider::class);
+        });
+    }
+}
