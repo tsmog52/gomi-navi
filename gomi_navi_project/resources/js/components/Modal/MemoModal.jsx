6cd6d9ca518dd663c8791b25337dfc0c323fa278
yumi <s_mog31@yahoo.co.jp>
2024-07-13 21:39:35 +0900

    feat:api経由でメモ送信機能追加

--- gomi_navi_project/resources/js/components/Modal/MemoModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/MemoModal.jsx
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
 import CloseButton from '../Button/CloseButton';
+import axios from 'axios';
 
 const MemoModal = ({isOpen, onClose}) => {
   const [inputText, setInputText] = useState('');
+
   const onChange = (e) => {
     setInputText(e.target.value);
-    console.log(inputText);
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //内容をAPIに送信してDBに保存する
+    try {
+      const response = await axios.post('/api/memos', { note: inputText });
+      console.log('保存しました:', response.data);
+      console.log(inputText);
+      //入力欄をクリアにする
+      // setInputText('');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+
   return (
     <>
       <div
         className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
       >
         <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div className='text-center'>
               <CloseButton onClose={onClose}/>
                 <input
@@ -25,6 +42,7 @@ const MemoModal = ({isOpen, onClose}) => {
                 />
             </div>
             <button
+              type='submit'
               className="bg-gray-600 mt-4 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded border"
             >
               メモを保存する
