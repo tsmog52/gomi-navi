09e0ce991e4159254c5ad630dc3eb89bd2ec5b9c
yumi <s_mog31@yahoo.co.jp>
2024-07-21 15:09:24 +0900

    disable:APIトークンの無効化

--- gomi_navi_project/app/Providers/AppServiceProvider.php
+++ gomi_navi_project/app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
-
+        // Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+            // Log::info('SocialiteWasCalled event fired');
             $event->extendSocialite('line', \SocialiteProviders\Line\Provider::class);
         });
     }
