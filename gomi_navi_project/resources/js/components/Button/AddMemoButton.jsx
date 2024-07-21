ba276abf6740489e607527a0ea983ccb2334c395
yumi <s_mog31@yahoo.co.jp>
2024-07-21 14:46:34 +0900

    clean:コード整形

--- gomi_navi_project/resources/js/components/Button/AddMemoButton.jsx
+++ gomi_navi_project/resources/js/components/Button/AddMemoButton.jsx
@@ -4,13 +4,18 @@ import useModal from '../../hooks/useModal';
 import MemoModal from '../Modal/MemoModal';
 
 const AddMemoButton = () => {
-  const {isOpen, open, close} = useModal(true);
+  const {isOpen, open, close} = useModal();
 
   return (
     <div>
       <button onClick={open}>
         <BsPlusSquare size={20} />
-        {isOpen && <MemoModal isOpen={isOpen}/>}
+        {isOpen &&
+        <MemoModal
+          isOpen={isOpen}
+          onClose={close}
+          text="メモを作成"
+        />}
       </button>
     </div>
   )
