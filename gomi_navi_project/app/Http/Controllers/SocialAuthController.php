e265262c9671f9e5f64c95c16b0c08af97b71839
yumi <s_mog31@yahoo.co.jp>
2024-07-21 15:08:16 +0900

    feat:access_typeパラメーターの追加

--- gomi_navi_project/app/Http/Controllers/SocialAuthController.php
+++ gomi_navi_project/app/Http/Controllers/SocialAuthController.php
@@ -14,65 +14,65 @@ class SocialAuthController extends Controller
     //共通化させる
     public function redirectToGoogle()
     {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')
+            ->with(['access_type' => 'offline'])
+            ->redirect();
     }
 
     public function handleGoogleCallback()
-    {
-        try {
-            $googleUser = Socialite::driver('google')->stateless()->user();
+{
+    try {
+        $googleUser = Socialite::driver('google')->stateless()->user();
 
-            // 既存のユーザーの検索または新規作成
-            $socialAccount = SocialAccount::updateOrCreate(
-                [
-                    'provider_id' => $googleUser->getId(),
-                    'provider' => 'google',
-                ],
-                [
-                    'google_token' => $googleUser->token,
-                    'google_refresh_token' => $googleUser->refreshToken,
-                ]
-            );
+        // 既存のユーザーの検索または新規作成(リフレッシュトークンの保存)
+        $socialAccount = SocialAccount::updateOrCreate(
+            [
+                'provider_id' => $googleUser->getId(),
+                'provider' => 'google',
+            ],
+            [
+                'google_token' => $googleUser->token,
+                'google_refresh_token' => $googleUser->refreshToken,
+            ]
+        );
 
-            // ユーザーの取得
-            $user = $socialAccount->user;
+        // ユーザーの取得
+        $user = $socialAccount->user;
 
-            // 新しいユーザーの取得
-            if (!$user) {
-                $user = User::create();
-            }
-
-            // user_id を設定する
-            $socialAccount->user_id = $user->id;
-            $socialAccount->save();
-
-            // PersonalAccessTokenの検索または新規作成
-            $personalAccessToken = SocialPersonalAccessToken::updateOrCreate(
-                [
-                    'user_id' => $user->id,
-                ],
-                [
-                    'token' => $googleUser->token,
-                    'refresh_token' => $googleUser->refreshToken ?? null,
-                    'expires_at' => Carbon::now()->addSeconds($googleUser->expiresIn),
-                ]
-            );
-
-            // ユーザーログイン
-            Auth::login($user);
-
-            // トークンの作成
-            $token = $user->createToken('auth_token')->plainTextToken;
-
-            // クッキーにトークンを設定（24時間有効）
-            $cookie = cookie('auth_token', $token, 1, null, null, false, true); // 1分（24時間）有効, HTTPOnly
-
-            // メインページにリダイレクト
-            return redirect('http://127.0.0.1:8000')->cookie($cookie);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Error in Google callback: ' . $e->getMessage()], 500);
+        // 新しいユーザーの取得
+        if (!$user) {
+            $user = User::create();
         }
+
+        // user_id を設定する
+        $socialAccount->user_id = $user->id;
+        $socialAccount->save();
+
+        // PersonalAccessTokenの検索または新規作成
+        $personalAccessToken = SocialPersonalAccessToken::updateOrCreate(
+            [
+                'user_id' => $user->id,
+            ],
+            [
+                'token' => $googleUser->token,
+                'refresh_token' => $googleUser->refreshToken ?? null,
+                'expires_at' => Carbon::now()->addSeconds($googleUser->expiresIn),
+            ]
+        );
+
+        // ユーザーログイン
+        Auth::login($user);
+
+        //フロントエンドのURLにリダイレクトさせる
+        $redirectUrl = 'http://127.0.0.1:8000/';
+
+        // JSONレスポンスを返す
+        return redirect()->to($redirectUrl)
+        ->with('user_id', $user->id);
+    } catch (\Exception $e) {
+        dd($e); // エラーが発生した場合、エラー情報を表示
     }
+}
 
     public function redirectToLine()
     {
@@ -81,59 +81,56 @@ class SocialAuthController extends Controller
 
     public function handleLineCallback()
     {
-        try {
-            $lineUser = Socialite::driver('line')->stateless()->user();
+        // try {
+        //     $lineUser = Socialite::driver('line')->user();
 
-            // 既存のユーザーの検索または新規作成
-            $socialAccount = SocialAccount::updateOrCreate(
-                [
-                    'provider_id' => $lineUser->getId(),
-                    'provider' => 'line',
-                ],
-                [
-                    'line_token' => $lineUser->token,
-                    'line_refresh_token' => $lineUser->refreshToken,
-                ]
-            );
+        //     // 既存のユーザーの検索または新規作成
+        //     $socialAccount = SocialAccount::updateOrCreate(
+        //         [
+        //             'provider_id' => $lineUser->getId(),
+        //             'provider' => 'line',
+        //         ],
+        //         [
+        //             'line_token' => $lineUser->token,
+        //             'line_refresh_token' => $lineUser->refreshToken,
+        //         ]
+        //     );
 
-            // ユーザーの取得
-            $user = $socialAccount->user;
+        //     // ユーザーの取得
+        //     $user = $socialAccount->user;
 
-            // 新しいユーザーの取得
-            if (!$user) {
-                $user = User::create();
-            }
+        //     // 新しいユーザーの取得
+        //     if (!$user) {
+        //         $user = User::create();
+        //     }
 
-            // user_id を設定する
-            $socialAccount->user_id = $user->id;
-            $socialAccount->save();
+        //     // user_id を設定する
+        //     $socialAccount->user_id = $user->id;
+        //     $socialAccount->save();
 
-            // PersonalAccessTokenの検索または新規作成
-            $personalAccessToken = SocialPersonalAccessToken::updateOrCreate(
-                [
-                    'user_id' => $user->id,
-                ],
-                [
-                    'token' => $lineUser->token,
-                    'refresh_token' => $lineUser->refreshToken ?? null,
-                    'expires_at' => Carbon::now()->addSeconds($lineUser->expiresIn),
-                ]
-            );
+        //     // PersonalAccessTokenの検索または新規作成
+        //     $personalAccessToken = SocialPersonalAccessToken::updateOrCreate(
+        //         [
+        //             'user_id' => $user->id,
+        //         ],
+        //         [
+        //             'token' => $lineUser->token,
+        //             'refresh_token' => $lineUser->refreshToken ?? null,
+        //             'expires_at' => Carbon::now()->addSeconds($lineUser->expiresIn),
+        //         ]
+        //     );
 
-            // ユーザーログイン
-            Auth::login($user);
+        //     // ユーザーログイン
+        //     Auth::login($user);
 
-            // トークンの発行
-            $token = $user->createToken('auth_token')->plainTextToken;
+        //     // メインページにリダイレクト
+        //     return redirect('http://127.0.0.1:8000');
+        // } catch (\Exception $e) {
+        //     return response()->json(['error' => 'Error in line callback: ' . $e->getMessage()], 500);
+        // }
+        $user = Socialite::driver('line')->user();
 
-            // クッキーにトークンを設定（24時間有効）
-            $cookie = cookie('auth_token', $token, 1440, null, null, false, true); // 1440分（24時間）有効, HTTPOnly
-
-            // メインページにリダイレクト
-            return redirect('http://127.0.0.1:8000')->cookie($cookie);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Error in line callback: ' . $e->getMessage()], 500);
-        }
+        dd($user);
     }
 
 }
