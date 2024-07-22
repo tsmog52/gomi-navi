375cbcb1e70514c4ea17f6c72c92224aaa339a77
yumi <s_mog31@yahoo.co.jp>
2024-07-23 08:54:11 +0900

    clean:コード整形

--- gomi_navi_project/config/services.php
+++ gomi_navi_project/config/services.php
@@ -13,6 +13,7 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
+
     'line' => [
         'client_id' => env('LINE_CLIENT_ID'),
         'client_secret' => env('LINE_CLIENT_SECRET'),
