1199d47775a90b4b2078255077d36cb1db7feb61
yumi <s_mog31@yahoo.co.jp>
2024-07-23 08:53:44 +0900

    feat:利用規約、プライバシーポリシーの作成

--- gomi_navi_project/resources/js/components/Footer.jsx
+++ gomi_navi_project/resources/js/components/Footer.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import PrivacyPolicy from '../PrivacyPolicy'
 
 const Footer = () => {
   return (
-    <>
-      <div className='bg-gray-200 p-4'>
-        <p className='flex items-center justify-center text-sm'>
-          <span className='pr-1'>&copy;</span>gomi-navi
+    <footer className='bg-gray-200 p-4 mt-auto'>
+      <div className='container mx-auto flex flex-col items-center'>
+        <ul className='flex space-x-4 mb-2'>
+          <li>
+            <a
+              href="/terms"
+              className='text-gray-600 hover:text-gray-400 transition-colors duration-300'
+            >
+              利用規約
+            </a>
+          </li>
+          <li>
+            <a
+              href="/privacy-policy"
+              className='text-gray-600 hover:text-gray-400 transition-colors duration-300'
+            >
+              プライバシーポリシー
+            </a>
+          </li>
+        </ul>
+        <p className='text-sm text-gray-600'>
+          <span className='pr-1'>&copy;</span>2024 gomi-navi
         </p>
       </div>
-    </>
+    </footer>
   )
 }
 
 export default Footer
+
