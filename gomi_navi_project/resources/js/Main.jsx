c0404d808088773c136ff9ec45688a1d19de3d85
yumi <s_mog31@yahoo.co.jp>
2024-07-25 07:29:31 +0900

    update:App.jsxからMain.jsxへ変更

---
+++ gomi_navi_project/resources/js/Main.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { FaTruck, FaSearch, FaCalendarAlt } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
+import { AiFillDelete } from "react-icons/ai";
+import { IoSettingsSharp } from "react-icons/io5";
+import Header from './components/Header';
+import useModal from './hooks/useModal';
+import SettingModal from './components/Modal/SettingModal';
+import PickupModal from './components/Modal/PickupModal';
+import MainCard from './components/Card/MainCard';
+import Footer from './components/Footer';
+import useAuth from './hooks/useAuth';
+
+const Main = () => {
+  // モーダル1:今日のゴミ回収
+  const { isOpen: isOpenModal1, open: openModal1, close: closeModal1 } = useModal();
+   // モーダル2:設定
+  const { isOpen: isOpenModal2, open: openModal2, close: closeModal2 } = useModal();
+
+  useAuth();
+
+  return (
+    <>
+      <Header />
+      <div className='flex justify-center items-center m-16'>
+        <main className="grid grid-cols-3 gap-16 items-center mt-10">
+          <MainCard
+            icon={FaTruck}
+            text='今日のゴミ回収'
+            onClick={openModal1}
+            size={60}
+          />
+          {isOpenModal1 && <PickupModal onClose={closeModal1} />}
+          <MainCard
+            icon={FaSearch}
+            link={'/item'}
+            text='ゴミの分別検索'
+            size={50}
+          />
+          <MainCard
+            icon={FaCalendarAlt}
+            link={'/calendar'}
+            text='カレンダー'
+            size={50}
+          />
+          <MainCard
+            icon={AiFillDelete}
+            link={'/category'}
+            text='ゴミの捨て方'
+            size={60}
+          />
+          <MainCard
+            icon={FaPhone}
+            link={'/contact'}
+            text='お問い合わせ'
+            size={50}
+          />
+          <MainCard
+            icon={IoSettingsSharp}
+            text='設定'
+            onClick={openModal2}
+            size={60}
+          />
+          {isOpenModal2 && <SettingModal onClose={closeModal2} />}
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
