8bcd68a170932451c9c98e4bfe0d10309636ccb5
yumi <s_mog31@yahoo.co.jp>
2024-07-25 07:32:44 +0900

    change:データの削除をローカルストレージからクッキーへ変更

--- gomi_navi_project/resources/js/components/Button/LogoutButton.jsx
+++ gomi_navi_project/resources/js/components/Button/LogoutButton.jsx
@@ -4,18 +4,13 @@ import Cookies from 'js-cookie';
 
 const LogoutButton = () => {
 
-  const deleteCookie = (name) => {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 2000 00:00:00 UTC; path=/;`;
-  };
-
   const handleLogout = async () => {
     try {
-      // ローカルストレージからトークンを削除
-      localStorage.removeItem('accessToken');
       // クッキーを削除
       Cookies.remove('auth_token');
+      Cookies.remove('user_id');
       // API経由でログアウト処理を行う(バックエンドのログアウトAPIのエンドポイントを指定)
-      await axios.post('http://127.0.0.1:8000/api/logout', {}, {
+      await axios.post('/api/logout', {}, {
         withCredentials: true
       });
       // ログアウト後のリダイレクト
