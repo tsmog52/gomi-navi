d37fee199bf9b2b125a4924242ca5af346a51cb0
yumi <s_mog31@yahoo.co.jp>
2024-07-24 19:21:06 +0900

    feat:LoginModalの作成

---
+++ gomi_navi_project/resources/js/components/Modal/LoginModal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import SocialMediaButton from '../Button/SocialMediaButton';
+import CloseButton  from '../Button/CloseButton';
+
+const LoginModal = ({title, linkText, onClose, google, line }) => {
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+          <div className='text-center'>
+            <CloseButton onClose={onClose}/>
+            <h2 className="block text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-4">
+              {title}
+            </h2>
+            <SocialMediaButton text={google} socialLink={"/login/google"}/>
+            <SocialMediaButton text={line} socialLink={"/auth/line"}/>
+              <p className="mb-4">
+                <a className="text-blue-600 underline hover:text-blue-400" href="#">
+                  {linkText}
+                </a>
+                </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LoginModal
