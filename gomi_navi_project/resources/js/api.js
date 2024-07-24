688d103552bef946ca8bd305144e1e5525871850
yumi <s_mog31@yahoo.co.jp>
2024-07-25 07:30:12 +0900

    feat:クッキーからデータを取得する関数を追加

--- gomi_navi_project/resources/js/api.js
+++ gomi_navi_project/resources/js/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
@@ -8,7 +8,7 @@ const axiosInstance = axios.create({
 });
 
 // CSRFトークンの取得
-const fetchCsrfCookie = async () => {
+export const fetchCsrfCookie = async () => {
   try {
     const response = await axiosInstance.get('/sanctum/csrf-cookie');
     console.log('CSRFトークンが設定されました', document.cookie);
@@ -16,7 +16,14 @@ const fetchCsrfCookie = async () => {
     console.error('CSRFトークンの取得に失敗しました:', error);
   }
 };
-//
+
+//クッキーからデータを取得
+export const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+};
 
 // メモの取得
 export const getMemos = async () => {
@@ -29,11 +36,11 @@ export const getMemos = async () => {
   }
 };
 
-export const postData = async (inputData) => {
+//メモの作成
+export const postMemo = async (inputData) => {
   try {
-    await fetchCsrfCookie();
+    // await fetchCsrfCookie();
     const response = await axiosInstance.post('/api/memo/create', inputData);
-    console.log('Success:', response.data);
     return response.data;
   } catch (error) {
     if (error.response) {
@@ -45,12 +52,11 @@ export const postData = async (inputData) => {
   }
 };
 
-
 // メモの更新
 export const updateMemo = async (updateMemo) => {
   try {
     await fetchCsrfCookie();
-    const response = await axiosInstance.patch(`/api/memo/${updateMemo.id}`, updateMemo);
+    const response = await axiosInstance.put(`/api/memo/${updateMemo.id}`, updateMemo);
     console.log(response.data);
   } catch (error) {
     console.error(error);
