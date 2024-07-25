b2c9e6775000e440e6a5d16d59e3755d7cc48f3c
yumi <s_mog31@yahoo.co.jp>
2024-07-25 13:04:46 +0900

    rename:ファイル名の修正

---
+++ gomi_navi_project/database/migrations/2024_01_01_000003_create_personal_access_tokens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('tokenable');
+            // アクセストークンを示す
+            $table->string('token', 255)->unique();
+            // ユーザーまたはクライアントが持つ権限や能力を示す
+            $table->text('abilities')->nullable();
+            // 最後にアクセストークンが使用された日時を示す
+            $table->timestamp('last_used_at')->nullable();
+            // アクセストークンの有効期限を示す
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+};
+
