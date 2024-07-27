81c22e1b8a012889fefe6e82e5518ddc7a2d198e
yumi <s_mog31@yahoo.co.jp>
2024-07-27 17:29:07 +0900

    feat:allItemsメソッドの追加

--- gomi_navi_project/app/Http/Controllers/ItemController.php
+++ gomi_navi_project/app/Http/Controllers/ItemController.php
@@ -1,40 +1,4 @@
 <?php
-// namespace App\Http\Controllers;
-
-// use App\Models\Item;
-// use Illuminate\Http\Response;
-
-// class ItemController extends Controller
-// {
-//     public function index() {
-//         $items = Item::paginate(9);
-
-//         // 取得したアイテムを整形
-//         $response = $items->items();
-//         $response = array_map(function ($item) {
-//             return [
-//                 'id' => $item->id,
-//                 'category_id' => $item->category_id,
-//                 'item_name' => $item->item_name,
-//                 'item_memo' => $item->item_memo,
-//                 'created_at' => $item->created_at,
-//                 'updated_at' => $item->updated_at,
-//                 'category_name' => $item->category->category_name ?? 'カテゴリなし'
-//             ];
-//         }, $response);
-
-//         // データをJSONに変換し、ページネーションの情報を含める
-//         return response()->json([
-//             'data' => $response,
-//             'current_page' => $items->currentPage(),
-//             'last_page' => $items->lastPage(),
-//             'next_page_url' => $items->nextPageUrl(),
-//             'prev_page_url' => $items->previousPageUrl(),
-//             'per_page' => $items->perPage(),
-//             'total' => $items->total(),
-//         ], Response::HTTP_OK);
-//     }
-// }
 
 namespace App\Http\Controllers;
 
@@ -78,4 +42,27 @@ class ItemController extends Controller
             'total' => $items->total(),
         ], Response::HTTP_OK);
     }
+
+    public function allItems() {
+        // 全てのアイテムを取得
+        $allItems = Item::all();
+
+        // アイテムデータを整形して返す
+        $response = $allItems->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'category_id' => $item->category_id,
+                'item_name' => $item->item_name,
+                'item_memo' => $item->item_memo,
+                'created_at' => $item->created_at,
+                'updated_at' => $item->updated_at,
+                'category_name' => $item->category->category_name ?? 'カテゴリなし'
+            ];
+        });
+
+        return response()->json([
+            'data' => $response,
+        ], Response::HTTP_OK);
+    }
+
 }
