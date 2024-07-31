8b0675b8cc0e75c74bd7eaf13ea3454178acfc33
yumi <s_mog31@yahoo.co.jp>
2024-07-31 21:37:07 +0900

    clean:コード整形

--- gomi_navi_project/resources/views/terms.blade.php
+++ gomi_navi_project/resources/views/terms.blade.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-  @vite('resources/js/components/TermsOfService.jsx')
+  @viteReactRefresh
+  @vite(['resources/js/components/TermsOfService.jsx','resources/css/app.css' ])
 </head>
 <body>
   <div id="terms"></div>
