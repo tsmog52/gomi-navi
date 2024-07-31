247be4a9033a72f1ae07ac5245c9da63fe861f28
yumi <s_mog31@yahoo.co.jp>
2024-07-31 21:56:22 +0900

    change:Routesの設定

--- gomi_navi_project/resources/js/app.jsx
+++ gomi_navi_project/resources/js/app.jsx
@@ -1,73 +1,13 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { FaTruck, FaSearch, FaCalendarAlt } from "react-icons/fa";
-import { FaPhone } from "react-icons/fa6";
-import { AiFillDelete } from "react-icons/ai";
-import { IoSettingsSharp } from "react-icons/io5";
-import Header from './components/Header';
-import useModal from './hooks/useModal';
-import SettingModal from './components/Modal/SettingModal';
-import PickupModal from './components/Modal/PickupModal';
-import MainCard from './components/Card/MainCard';
-import Footer from './components/Footer';
-import Cookie from 'js-cookie';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Main from './components/Main';
 
-//ログイン回り
-export const AuthContext = createContext();
-
-const App = () => {
-  // モーダル1:今日のゴミ回収(分割代入)
-  const { isOpen: isOpenModal1, open: openModal1, close: closeModal1 } = useModal();
-  // モーダル2:設定(分割代入)
-  const { isOpen: isOpenModal2, open: openModal2, close: closeModal2 } = useModal();
-
+function App() {
   return (
-    <>
-      <Header />
-      <div className='flex justify-center items-center m-16'>
-        <main className="grid grid-cols-3 gap-16 items-center mt-10">
-          <MainCard
-            icon={FaTruck}
-            text='今日のゴミ回収'
-            onClick={openModal1}
-            size={60}
-          />
-          {isOpenModal1 && <PickupModal onClose={closeModal1} />}
-          <MainCard
-            icon={FaSearch}
-            link={'/item'}
-            text='ゴミの分別検索'
-            size={50}
-          />
-          <MainCard
-            icon={FaCalendarAlt}
-            link={'/calendar'}
-            text='カレンダー'
-            size={50}
-          />
-          <MainCard
-            icon={AiFillDelete}
-            link={'/category'}
-            text='ゴミの捨て方'
-            size={60}
-          />
-          <MainCard
-            icon={FaPhone}
-            link={'/contact'}
-            text='お問い合わせ'
-            size={50}
-          />
-          <MainCard
-            icon={IoSettingsSharp}
-            text='設定'
-            onClick={openModal2}
-            size={60}
-          />
-          {isOpenModal2 && <SettingModal onClose={closeModal2} />}
-        </main>
-      </div>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
   );
-};
+}
 
 export default App;
