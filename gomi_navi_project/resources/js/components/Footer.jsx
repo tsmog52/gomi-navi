6d2c70b24dd17fe4032744ae49512f6201bc16c7
yumi <s_mog31@yahoo.co.jp>
2024-08-03 13:29:09 +0900

    feat:画面サイズに応じたstyleの付与機能を追加

--- gomi_navi_project/resources/js/components/Footer.jsx
+++ gomi_navi_project/resources/js/components/Footer.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { FaGithub } from "react-icons/fa6";
+import TermsAndPrivacy from './TermsAndPrivacy';
 
 const Footer = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 640); // sm以下のサイズをチェック
+    };
+
+    handleResize(); // 初期チェック
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <footer className='bg-gray-200 p-4 mt-auto'>
       <div className='container mx-auto flex flex-col items-center'>
-        <ul className='flex space-x-4 mb-2'>
-          <li>
-            <a
-              href="http://127.0.0.1:8000/terms"
-              className='text-gray-600 hover:text-gray-400 transition-colors duration-300'
-            >
-              利用規約
-            </a>
-          </li>
-          <li>
-            <a
-              href="http://127.0.0.1:8000/privacy"
-              className='text-gray-600 hover:text-gray-400 transition-colors duration-300'
-            >
-              プライバシーポリシー
-            </a>
-          </li>
-        </ul>
+      {!isMobile && <TermsAndPrivacy />}
         <div className='flex'>
           <p className='text-sm text-gray-600 pr-2'>
             <span className='pr-1'>&copy;</span>2024 gomi-navi
