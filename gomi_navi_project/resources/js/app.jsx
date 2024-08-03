33114aa12dcea658b8664e1c6e656bb9f237eaa2
yumi <s_mog31@yahoo.co.jp>
2024-08-03 13:59:06 +0900

    feat:RecoilRootの追加

--- gomi_navi_project/resources/js/app.jsx
+++ gomi_navi_project/resources/js/app.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
+import Calendar from './components/Calendar';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
+    <RecoilRoot>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/calendar" element={<Calendar />} />
+      </Routes>
+    </RecoilRoot>
   );
 }
 
