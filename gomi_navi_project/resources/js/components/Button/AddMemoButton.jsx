b2b4d989844592ac55fb2bb2f26a1abb0dd77067
yumi <s_mog31@yahoo.co.jp>
2024-08-05 20:22:39 +0900

    feat:メモを保存する関数を追加

--- gomi_navi_project/resources/js/components/Button/AddMemoButton.jsx
+++ gomi_navi_project/resources/js/components/Button/AddMemoButton.jsx
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { BsPlusSquare } from "react-icons/bs";
 import useModal from '../../hooks/useModal';
 import MemoModal from '../Modal/MemoModal';
+import { getMemos } from '../../api'; // APIからメモリストを取得する関数
+import { useRecoilState } from 'recoil';
+import { memoState } from '../../states/memoState';
 
 const AddMemoButton = () => {
-  const {isOpen, open, close} = useModal();
+  const { isOpen, open, close } = useModal();
+  const [memos, setMemos] = useRecoilState(memoState);
+
+  useEffect(() => {
+    // メモリストの変更を監視し、コンポーネントを更新する
+    const fetchMemos = async () => {
+      try {
+        const response = await getMemos();
+        setMemos(response.memos || []);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchMemos();
+  }, []);
+
+  const onSave = async (savedMemo) => {
+    try {
+      // メモの保存が成功した場合、メモリストを再取得して更新
+      const response = await getMemos();
+      console.log('Fetched memos on save:', response); // responseの内容をログに出力して確認
+      setMemos(response.memos || []);
+    } catch (error) {
+      console.error('Error fetching memos:', error.message);
+    }
+  };
 
   return (
     <div>
       <button onClick={open}>
         <BsPlusSquare size={20} />
         {isOpen &&
-        <MemoModal
-          isOpen={isOpen}
-          onClose={close}
-          text="メモを作成"
-        />}
+          <MemoModal
+            isOpen={isOpen}
+            onClose={close}
+            onSave={onSave}
+            text="メモを作成"
+          />}
       </button>
     </div>
-  )
+  );
 }
 
-export default AddMemoButton
+export default AddMemoButton;
+
