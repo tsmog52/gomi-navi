5bc911e01ee92c5ee12302ffd42fffcc499880db
yumi <s_mog31@yahoo.co.jp>
2024-08-05 20:26:21 +0900

    fix:Cookieからuser_idを取得するよう修正

--- gomi_navi_project/resources/js/states/loginState.js
+++ gomi_navi_project/resources/js/states/loginState.js
@@ -1,8 +1,8 @@
 import { atom } from "recoil";
 import Cookies from "js-cookie";
 
+// Cookieの値に基づく初期値を設定
 export const loginState = atom({
   key: "loginState",
-  default: !!Cookies.get('user_id'),
+  default: Cookies.get('user_id'),
 });
-
