4d757d5a59a0ebf6cfd0504735ae9f33e1ce4185
yumi <s_mog31@yahoo.co.jp>
2024-08-08 13:15:40 +0900

    fix:category_idのFK追加

--- gomi_navi_project/database/migrations/2024_01_01_000010_create_sorting_guides_table.php
+++ gomi_navi_project/database/migrations/2024_01_01_000010_create_sorting_guides_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->text('instruction');
             $table->timestamps();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
