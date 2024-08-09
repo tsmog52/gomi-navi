12946ba94ac5231cbc75e322583485775068c1a0
yumi <s_mog31@yahoo.co.jp>
2024-08-09 20:48:47 +0900

    clean:コード整形

--- gomi_navi_project/config/app.php
+++ gomi_navi_project/config/app.php
@@ -15,8 +15,6 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'url' => env('APP_URL', 'https://gomi-navi.net'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -54,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://gomi-navi.net'),
 
     /*
     |--------------------------------------------------------------------------
