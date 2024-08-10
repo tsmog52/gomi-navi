4a669879d41157db513307f6ea2b5b09969d885b
yumi <s_mog31@yahoo.co.jp>
2024-08-10 14:07:45 +0900

    feat:propsの追加

--- gomi_navi_project/resources/js/components/Button/GoogleButton.jsx
+++ gomi_navi_project/resources/js/components/Button/GoogleButton.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 
-const GoogleButton = ({ text }) => {
+const GoogleButton = ({ text, socialLink }) => {
   const navigate = useNavigate();
 
   const handleClick = async () => {
