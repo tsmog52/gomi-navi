f9044cfc2b9308c9faa2221395cf4a7b6ca159c5
yumi <s_mog31@yahoo.co.jp>
2024-08-11 09:28:31 +0900

    rename:モデル名の変更

--- gomi_navi_project/app/Models/PersonalAccessToken.php
+++ gomi_navi_project/app/Models/PersonalAccessToken.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SocialPersonalAccessToken extends Model
+class PersonalAccessToken extends Model
 {
     use HasFactory;
 
