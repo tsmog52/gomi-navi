284abc5babaa7d66561487a45afe4efd410a5654
yumi <s_mog31@yahoo.co.jp>
2024-08-11 17:46:25 +0900

    feat:ヘッダーにcsrfトークンを送信、navigate機能を追加

--- gomi_navi_project/resources/js/components/Button/LogoutButton.jsx
+++ gomi_navi_project/resources/js/components/Button/LogoutButton.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
+      // CSRFトークンを取得
+      const csrfToken = Cookies.get('XSRF-TOKEN');
       // クッキーを削除
       Cookies.remove('auth_token');
       Cookies.remove('user_id');
       // API経由でログアウト処理を行う(バックエンドのログアウトAPIのエンドポイントを指定)
       await axios.post('/api/logout', {}, {
-        withCredentials: true
+        withCredentials: true,
+        headers: {
+          'X-CSRF-TOKEN': csrfToken
+        }
       });
       // ログアウト後のリダイレクト
-      window.location.href = '/';
+      navigate('/');
     } catch (error) {
       console.error('Error logging out:', error);
     }
