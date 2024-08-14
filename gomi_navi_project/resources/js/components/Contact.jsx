240f343651c55c244ac200163270e5657dc886a7
yumi <s_mog31@yahoo.co.jp>
2024-08-14 11:18:00 +0900

    update:styleをAccordionへ変更

--- gomi_navi_project/resources/js/components/Contact.jsx
+++ gomi_navi_project/resources/js/components/Contact.jsx
@@ -1,51 +1,106 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Header from './Header';
+import React, { useState } from 'react';
 import ContactCard from './Card/ContactCard';
+import Header from './Header';
+import Footer from './Footer';
 
 const Contact = () => {
+  const [expanded, setExpanded] = useState(null);
+
+  const handleClick = (index) => {
+    setExpanded(expanded === index ? null : index);
+  };
+
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>
-        <div className="p-8 flex flex-col items-center">
-          <p className="text-4xl font-bold pt-4">お問い合わせ一覧</p>
-          <p className="pt-4 text-xl">ごみの分別や収集に関するお問い合わせは、
-            <br /><span className='pl-14'>生活環境事業所へお問い合わせください。</span></p>
+      <div className="flex flex-grow justify-center">
+        <div className="w-1/3 border-2 border-contact-bgc p-8  bg-contact-bgc">
+          <p className="text-center text-4xl font-bold p-4">お問い合わせ一覧</p>
+          <p className="text-xl p-4 text-center">
+            ごみの分別や収集に関するお問い合わせは、
+            <br />
+            <span className="pl-14">生活環境事業所へお問い合わせください。</span>
+          </p>
+
+          <div className="flex flex-col items-center">
+            <div>
+              <p
+                onClick={() => handleClick(0)}
+                className="cursor-pointer p-4 text-lg font-semibold text-gray-600 hover:text-gray-400 transition-colors duration-300 hover:underline"
+              >
+                生活環境事業所
+              </p>
+              {expanded === 0 && (
+                <ContactCard
+                  css="p-4 w-full border-2 rounded-lg"
+                  title="生活環境事業所"
+                  tel="044-200-2583"
+                  hours="午前8:00 - 午後4:45"
+                  note="※日曜日、1月1日から3日を除く"
+                />
+              )}
+            </div>
+
+            <div>
+              <p
+                onClick={() => handleClick(1)}
+                className="cursor-pointer p-4 text-lg font-semibold text-gray-600 hover:text-gray-400 transition-colors duration-300 hover:underline"
+              >
+                粗大ゴミ受付センター
+              </p>
+              {expanded === 1 && (
+                <ContactCard
+                  css="p-4 w-full border-2 rounded-lg"
+                  title="粗大ゴミ受付センター"
+                  tel="0570-044-530"
+                  hours="午前8:00 - 午後4:45"
+                  note="※日曜日、1月1日から3日を除く"
+                />
+              )}
+            </div>
+
+            <div>
+              <p
+                onClick={() => handleClick(2)}
+                className="cursor-pointer p-4 text-lg font-semibold text-gray-600 hover:text-gray-400 transition-colors duration-300 hover:underline"
+              >
+                ごみ・リサイクル関連施設
+              </p>
+              {expanded === 2 && (
+                <ContactCard
+                  css="p-4 w-full border-2 rounded-lg"
+                  title="ごみ・リサイクル関連施設"
+                  tel="012-345-6789"
+                  hours="午前8:00 - 午後4:45"
+                />
+              )}
+            </div>
+
+            <div>
+              <p
+                onClick={() => handleClick(3)}
+                className="cursor-pointer p-4 text-lg font-semibold text-gray-600 hover:text-gray-400 transition-colors duration-300 hover:underline"
+              >
+                川崎市ホームページ
+              </p>
+              {expanded === 3 && (
+                <ContactCard
+                  css="p-4 w-full border-2 rounded-lg"
+                  title="川崎市ホームページ"
+                  tel="044-200-2111（代表）"
+                  hours="午前8:30 - 午後5:00"
+                  url="https://www.city.kawasaki.jp/"
+                  note="※祝休日・12月29日から1月3日を除く"
+                />
+              )}
+            </div>
+          </div>
         </div>
-        <div className="grid grid-cols-2 gap-8 justify-center items-center px-8">
-          <ContactCard
-            css='p-4 w-full h-60 border-4 rounded-lg'
-            title='生活環境事業所'
-            tel='044-200-2583'
-            hours='午前8:00 - 午後4:45'
-            note='※日曜日、1月1日から3日を除く'
-          />
-          <ContactCard
-            css='p-4 w-full h-60 border-4 rounded-lg'
-            title='粗大ゴミ受付センター'
-            tel='0570-044-530'
-            hours='午前8:00 - 午後4:45'
-            note='※日曜日、1月1日から3日を除く'
-          />
-          <ContactCard
-            css='p-4 w-full h-60 border-4 rounded-lg'
-            title='ごみ・リサイクル関連施設'
-            tel='012-345-6789'
-            hours='午前8:00 - 午後4:45'
-          />
-          <ContactCard
-            css='p-4 w-full h-60 border-4 rounded-lg'
-            title='川崎市ホームページ'
-            tel='044-200-2111（代表）'
-            hours='午前8:30 - 午後5:00'
-            url='https://www.city.kawasaki.jp/'
-            note='※祝休日・12月29日から1月3日を除く'
-          />
-        </div>
-      </main>
-    </>
+      </div>
+      <Footer />
+    </div>
   );
 };
-export default Contact
+
+export default Contact;
 
