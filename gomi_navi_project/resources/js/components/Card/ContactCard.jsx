231b3296ae17968090dcb67622bbfc3ff5a2db6e
yumi <s_mog31@yahoo.co.jp>
2024-08-14 11:19:04 +0900

    change:propsの削除

--- gomi_navi_project/resources/js/components/Card/ContactCard.jsx
+++ gomi_navi_project/resources/js/components/Card/ContactCard.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-const ContactCard = ({ title, hours, tel, note, url, css }) => {
+const ContactCard = ({ hours, tel, note, url, css }) => {
   return (
     <>
       <div className={css}>
-        <h2 className="text-2xl font-bold p-4 text-center">{title}</h2>
         <div className='flex flex-col items-center justify-center'>
           <p className='text-lg pb-2'><strong>TEL:</strong> <span className='border-b-2'>{tel}</span></p>
           <p className='text-lg pb-2'><strong>受付時間:</strong> <span>{hours}</span></p>
