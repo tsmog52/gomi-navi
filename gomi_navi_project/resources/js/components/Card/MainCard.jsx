305d521bec1838febfcf84f520c28ffaee045485
yumi <s_mog31@yahoo.co.jp>
2024-08-16 21:18:02 +0900

    feat:モバイル対応の追加

--- gomi_navi_project/resources/js/components/Card/MainCard.jsx
+++ gomi_navi_project/resources/js/components/Card/MainCard.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-const MainCard = ({ text, icon:Icon, link, onClick, size }) => {
+const MainCard = ({ text, icon: Icon, link, onClick }) => {
   return (
     <>
       <button
-        className="w-60 h-40 border-2 border-black flex flex-col items-center justify-center rounded-lg" onClick={onClick}
+        className="border-2 border-black rounded-lg w-64 h-16 sm:w-60 sm:h-40
+                              flex items-center justify-between sm:justify-center px-4 sm:mt-4 sm:flex-col"
+        onClick={onClick}
       >
-        <a href={link}>
-          <Icon size={size} color={'#272f3f'} className="ml-4"/>
-          <p className='pt-4'>{text}</p>
+        <a href={link} className="flex items-center w-full sm:flex-col">
+          <Icon
+            size={24}
+            className="sm:size-14 text-gray-700"
+          />
+          <p
+            className="flex-grow text-center text-x font-bold sm:text-lg sm:pt-4 sm:font-medium"
+          >
+            {text}
+          </p>
         </a>
       </button>
     </>
-  )
+  );
 }
 
-export default MainCard
+export default MainCard;
+
