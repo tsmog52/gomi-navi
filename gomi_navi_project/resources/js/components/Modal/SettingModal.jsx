178f5a43d39bf1eb515d9a6e914b598c012569a6
yumi <s_mog31@yahoo.co.jp>
2024-08-17 16:12:19 +0900

    feat:モバイル対応の追加

--- gomi_navi_project/resources/js/components/Modal/SettingModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/SettingModal.jsx
@@ -1,27 +1,14 @@
 import React, { useState } from 'react';
-import SocialMediaButton from '../Button/GoogleButton';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from 'react-router-dom';
 import CloseButton from '../Button/CloseButton';
-import axios from 'axios';
 import LineButton from '../Button/LineButton';
-import GoogleButton from '../Button/GoogleButton';
 
-const regions = ['川崎区', '中原区', '高津区', '宮前区', '多摩区', '麻生区', '幸区'];
 
 const SettingModal = ({ onClose }) => {
+  const regions = ['川崎区', '中原区', '高津区', '宮前区', '多摩区', '麻生区', '幸区'];
   //多摩区で登録
   const [selected, setSelected] = useState(regions[4]);
-
-  // カレンダーとの連携
-  const handleAddEvent = async () => {
-    try {
-      const response = await axios.post('/api/calendar/event/add');
-      console.log('Event added:', response.data);
-      alert('イベントが追加されました。');
-    } catch (error) {
-      console.error('Error adding event:', error);
-      alert('イベントの追加中にエラーが発生しました。');
-    }
-  };
 
   return (
     <>
@@ -33,9 +20,9 @@ const SettingModal = ({ onClose }) => {
             </h2>
             <CloseButton onClose={onClose} />
           </div>
-          <div className="border-b-2 my-4"></div>
-          <p className='pt-4 text-center font-bold text-lg sm:text-xl'>地域を設定する</p>
-          <div className="mt-4 grid grid-cols-2 gap-2 p-2">
+          <div className="border-b-2 sm:my-4"></div>
+          <p className='text-base pt-2 text-center font-bold text-lg sm:text-xl'>地域を設定する</p>
+          <div className="grid grid-cols-2 gap-2 p-2">
             {regions.map((region) => (
               <button
                 key={region}
@@ -47,15 +34,48 @@ const SettingModal = ({ onClose }) => {
               </button>
             ))}
           </div>
-          <p className='text-center font-bold text-lg sm:text-xl p-4'>外部リマインダー連携</p>
-          <LineButton
-            text='LINEと連携する'
-            socialLink={"/auth/line"}
-          />
+          <div className='hidden sm:block'>
+            <p className='text-base text-center font-bold text-lg sm:text-xl p-2 sm:p-4'>外部リマインダー連携</p>
+            <LineButton
+              text='LINEと連携する'
+              socialLink={"/auth/line"}
+            />
+          </div>
+          <div className='sm:hidden'>
+            <ul className='flex flex-col mb-2 pt-2'>
+              <li className='border-t border-gray-300 py-2 flex items-center justify-between'>
+                <Link
+                  to="/auth/line"
+                  className='text-gray-600 hover:text-gray-400 transition-colors duration-300 text-lg font-medium'
+                >
+                  LINEと連携する
+                </Link>
+                <MdOutlineKeyboardArrowRight className='text-gray-600' />
+              </li>
+              <li className='border-t border-gray-300 py-2 flex items-center justify-between'>
+                <Link
+                  to="/terms"
+                  className='text-gray-600 hover:text-gray-400 transition-colors duration-300 text-lg font-medium'
+                >
+                  利用規約
+                </Link>
+                <MdOutlineKeyboardArrowRight className='text-gray-600' />
+              </li>
+              <li className='border-t border-gray-300 py-2 flex items-center justify-between border-b'>
+                <Link
+                  to="/privacy"
+                  className='text-gray-600 hover:text-gray-400 transition-colors duration-300 text-lg font-medium'
+                >
+                  プライバシーポリシー
+                </Link>
+                <MdOutlineKeyboardArrowRight className='text-gray-600' />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-export default SettingModal
+export default SettingModal;
