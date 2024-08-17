93e86ac347b7b275dd1a498a311483f589281ad2
yumi <s_mog31@yahoo.co.jp>
2024-08-17 16:12:37 +0900

    style:モバイル対応の追加

--- gomi_navi_project/resources/js/components/Card/MainCard.jsx
+++ gomi_navi_project/resources/js/components/Card/MainCard.jsx
@@ -4,20 +4,25 @@ const MainCard = ({ text, icon: Icon, link, onClick }) => {
   return (
     <>
       <button
-        className="border-2 border-black rounded-lg w-64 h-16 sm:w-60 sm:h-40
+        className="relative border-2 border-black sm:rounded-lg w-64 h-16 sm:w-60 sm:h-40
                               flex items-center justify-between sm:justify-center px-4 sm:mt-4 sm:flex-col"
         onClick={onClick}
       >
-        <a href={link} className="flex items-center w-full sm:flex-col">
-          <Icon
-            size={24}
-            className="sm:size-14 text-gray-700"
-          />
-          <p
-            className="flex-grow text-center text-x font-bold sm:text-lg sm:pt-4 sm:font-medium"
-          >
-            {text}
-          </p>
+        <div className="absolute left-0 top-0 w-1/4 h-full bg-gray-300 sm:bg-transparent"></div>
+        <a href={link} className="flex w-full sm:flex-col relative z-10">
+          <div className="w-1/3 flex items-center justify-start pl-2 sm:w-full sm:justify-center">
+            <Icon
+              size={26}
+              className="sm:size-14 text-gray-700"
+            />
+          </div>
+          <div className="w-2/3 flex items-center justify-center sm:w-full sm:justify-center">
+            <p
+              className="text-center text-x font-bold sm:text-lg sm:pt-4 sm:font-medium"
+            >
+              {text}
+            </p>
+          </div>
         </a>
       </button>
     </>
@@ -25,4 +30,5 @@ const MainCard = ({ text, icon: Icon, link, onClick }) => {
 }
 
 export default MainCard;
+
 
