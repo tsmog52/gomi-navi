e848dac3ccfadbdd52189dd4070713f466bc9142
yumi <s_mog31@yahoo.co.jp>
2024-08-18 13:41:00 +0900

    style:モバイル版はitemの下にModalが表示されるよう修正

--- gomi_navi_project/resources/js/components/Category.jsx
+++ gomi_navi_project/resources/js/components/Category.jsx
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { createRoot } from 'react-dom/client';
 import Header from './Header.jsx';
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import useAccordion from '../hooks/useAccordion.jsx';
+import Footer from './Footer.jsx';
 
 const Category = () => {
   const [guides, setGuides] = useState([]);
-  //個別にアコーディオンを管理
   const {isAccordion, toggleAccordion} = useAccordion(null);
 
   useEffect(() => {
@@ -25,22 +24,33 @@ const Category = () => {
   return (
     <>
       <Header />
-      <div className='flex'>
-        <div className='w-2/5'>
+      <div className='w-full sm:flex'>
+        <div className='w-full sm:w-2/5'>
           <ul>
             {guides.map((guide, index) => (
               <li key={index}>
-                <button onClick={() => toggleAccordion(index)} className='flex justify-between w-full p-4 text-2xl'>
+                <button
+                  onClick={() => toggleAccordion(index)}
+                  className='flex justify-between w-full p-4 text-2xl'
+                >
                   {guide.category_name}
                   <MdOutlineKeyboardArrowRight size={30} />
                 </button>
                 <div className='border-b-2'></div>
+                {isAccordion === index && (
+                  <div className='block sm:hidden p-4'>
+                    <div className='border-2 p-2'>
+                      <p className='text-2xl font-normal pb-2'>出し方</p>
+                      {guide.instructions}
+                    </div>
+                  </div>
+                )}
               </li>
             ))}
           </ul>
         </div>
-        <div className='w-3/5 bg-gray-100 h-screen flex justify-center items-center'>
-          <div className='w-96 bg-white flex justify-center items-center'>
+        <div className='hidden sm:w-3/5 sm:flex bg-gray-100 h-screen justify-center items-center'>
+          <div className='sm:w-96 bg-white flex justify-center items-center'>
             {guides.map((guide, index) => (
               isAccordion === index && (
               <div className='w-112 h-128 bg-white rounded-lg p-4'>
@@ -62,8 +72,10 @@ const Category = () => {
           </div>
         </div>
       </div>
-  </>
+      <Footer />
+    </>
   );
 };
+
 export default Category;
 
