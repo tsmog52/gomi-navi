a228ff76dcddd41f5321e9b2c491ec999ff047ab
yumi <s_mog31@yahoo.co.jp>
2024-08-22 19:56:34 +0900

    delete:プライバシーポリシーと利用規約を削除

--- gomi_navi_project/resources/js/components/Footer.jsx
+++ gomi_navi_project/resources/js/components/Footer.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import { FaGithub } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
 
 const Footer = ({ className }) => {
   return (
     <footer className={` p-4 mt-auto ${className}`}>
       <div className='container mx-auto flex flex-col items-center'>
-        {/* <ul className='flex space-x-4 mb-2 hidden md:flex'>
-          <li>
-            <Link
-              to="/terms"
-              className='text-gray-600 hover:text-gray-400 transition-colors duration-300'
-            >
-              利用規約
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/privacy"
-              className='text-gray-600 hover:text-gray-400 transition-colors duration-300'
-            >
-              プライバシーポリシー
-            </Link>
-          </li>
-        </ul> */}
         <div className='flex'>
           <p className='text-sm text-gray-600 pr-2'>
             <span className='pr-1'>&copy;</span>2024 gomi-navi
