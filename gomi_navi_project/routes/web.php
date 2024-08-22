245f0de864eba4243c5d2dda134acba96474dc8d
yumi <s_mog31@yahoo.co.jp>
2024-08-23 07:48:52 +0900

    fix:ルートの修正

--- gomi_navi_project/routes/web.php
+++ gomi_navi_project/routes/web.php
@@ -16,8 +16,8 @@ Route::get('login/google', [GoogleController::class, 'redirectToGoogle']);
 Route::get('login/google/callback', [GoogleController::class, 'handleGoogleCallback']);
 
 // LINEAuthのルート
-Route::get('auth/line', [LineController::class, 'redirectToLine']);
-Route::get('auth/line/callback', [LineController::class, 'callback']);
+Route::get('/auth/line', [LineController::class, 'redirectToLine']);
+Route::get('/auth/line/callback', [LineController::class, 'callback']);
 
 //LINEメッセージ受信・送信用
 Route::post('/line/webhook', [LineBotController::class, 'reply'])
