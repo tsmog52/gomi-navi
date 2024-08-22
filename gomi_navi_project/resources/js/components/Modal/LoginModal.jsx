4cb32a185e16933e63a03d09c664a8c492f70058
yumi <s_mog31@yahoo.co.jp>
2024-08-22 19:38:22 +0900

    style:ボタンの大きさ、背景色をgreenへ変更

--- gomi_navi_project/resources/js/components/Modal/LoginModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/LoginModal.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import CloseButton  from '../Button/CloseButton';
-// import GoogleButton from '../Button/GoogleButton';
+import CloseButton from '../Button/CloseButton';
 import LineButton from '../Button/LineButton';
 
-const LoginModal = ({title, onClose, google, line }) => {
+const LoginModal = ({ title, onClose, google, line }) => {
   return (
     <>
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4 md:p-4">
+        <div className="bg-white rounded-lg shadow-lg max-w-sm md:max-w-lg w-full p-6 md:p-4">
           <div className='text-center'>
-            <CloseButton onClose={onClose}/>
-            <h2 className="block text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-4">
+            <CloseButton onClose={onClose} />
+            <h2 className="text-xl md:text-2xl font-bold text-gray-800 dark:text-neutral-200 pb-2 md:pb-4">
               {title}
             </h2>
-            {/* <GoogleButton text={google} socialLink={"/login/google"}/> */}
-            <p className='pb-2'>連携することでLINEアカウントで<br />リマインダー通知を受け取ることができます。</p>
-            <LineButton text={line} socialLink={"/auth/line"}/>
+            <p className='text-sm md:text-base pb-2'>
+              LINEアカウントでリマインダー通知を<br />受け取ることができます。
+            </p>
+            <LineButton text={line} socialLink={'/auth/line'} />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default LoginModal
+export default LoginModal;
+
+
