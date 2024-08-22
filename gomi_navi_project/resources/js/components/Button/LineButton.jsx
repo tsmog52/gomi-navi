e66dd72d2c41ead9b3f90ea06607f6d5ad755b98
yumi <s_mog31@yahoo.co.jp>
2024-08-23 06:56:10 +0900

    fix:bg-transparentの削除

--- gomi_navi_project/resources/js/components/Button/LineButton.jsx
+++ gomi_navi_project/resources/js/components/Button/LineButton.jsx
@@ -12,7 +12,7 @@ const LineButton = ({ text, socialLink }) => {
     <div className='w-full flex items-center justify-center'>
       <button
         onClick={handleClick}
-        className='w-full h-[40px] px-4 py-2 sm:h-[50px] sm:px-6 sm:py-3 sm:w-auto sm:min-w-[300px] md:min-w-[384px] rounded-lg bg-green-500 bg-transparent flex items-center justify-center gap-2 cursor-pointer mb-4 hover:bg-green-600'
+        className='w-full h-[40px] px-4 py-2 sm:h-[50px] sm:px-6 sm:py-3 sm:w-auto sm:min-w-[300px] md:min-w-[384px] rounded-lg bg-green-500 flex items-center justify-center gap-2 cursor-pointer mb-4 hover:bg-green-600'
       >
         <FaRegBell
           className='text-white'
