e4a36c77c694be65d3c012ea791f391e977e5244
yumi <s_mog31@yahoo.co.jp>
2024-08-23 10:16:20 +0900

    style:コード整形

--- gomi_navi_project/resources/js/components/ItemFilter.jsx
+++ gomi_navi_project/resources/js/components/ItemFilter.jsx
@@ -81,41 +81,36 @@ import React, { useState, useEffect } from 'react';
 import { FaFilter } from "react-icons/fa";
 import axios from 'axios';
 
-const ItemFilter = () => {
-  const categories = ['燃えるゴミ', '空き缶・ペットボトル', '空きビン', '使用済み乾電池', 'ミックスペーパー', 'プラスチック', '小物金属', 'ダンボール', '粗大ゴミ', '収集品目外'];
+const ItemFilter = ({ onItemsFetched }) => {
+  const categories = ['普通ゴミ', '空き缶・ペットボトル', '空きビン', '使用済み乾電池', 'ミックスペーパー', 'プラスチック', '小物金属', 'ダンボール', '粗大ゴミ', '収集品目外'];
 
   const [itemsByCategory, setItemsByCategory] = useState({});
   const [selectedCategory, setSelectedCategory] = useState(null);
 
+  const handleFilter = (category) => {
+    setSelectedCategory(category);
+    if (onItemsFetched) {
+      onItemsFetched(itemsByCategory[category] || []);
+    }
+  };
+
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        const response = await axios.get('/api/items');
+        const response = await axios.get('/api/items/all');
         const items = response.data.data;
-
         // カテゴリーごとにアイテムをフィルタリング
         const categorizedItems = categories.reduce((acc, category) => {
           acc[category] = items.filter(item => item.category_name === category);
           return acc;
         }, {});
-
         setItemsByCategory(categorizedItems);
-
       } catch (error) {
         console.log(error.message);
       }
     };
-
     fetchItems();
   }, []);
-
-  const handleFilter = (category) => {
-    setSelectedCategory(category);
-    console.log(selectedCategory);
-    
-    console.log(itemsByCategory[category]);
-  };
-
 
   return (
     <>
@@ -128,7 +123,7 @@ const ItemFilter = () => {
           <button
             key={category}
             onClick={() => handleFilter(category)}
-            className='bg-gray-200 m-1 px-5 py-1 rounded-xl hover:bg-gray-300'
+            className='bg-white border m-1 px-5 py-1 rounded-xl hover:bg-gray-300'
           >
             {category}
           </button>
