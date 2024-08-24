19e6f4db4b3dbf00f974c60696a8ddf80314badc
yumi <s_mog31@yahoo.co.jp>
2024-08-24 20:40:33 +0900

    style:矢印を180度回転させる

--- gomi_navi_project/resources/js/components/Item.jsx
+++ gomi_navi_project/resources/js/components/Item.jsx
@@ -106,7 +106,7 @@ const Item = () => {
             placeholder="検索"
             onClick={handleClick}
           />
-          <ItemFilter  onItemsFetched={handleItemsFetched}/>
+          <ItemFilter onItemsFetched={handleItemsFetched} />
           <ul>
             {showDetailSearch ? (
               filteredValue.length === 0 ? (
@@ -115,41 +115,43 @@ const Item = () => {
                 </div>
               ) : (
                 filteredValue.map((item, index) => (
-                  <>
+                  <li key={index}>
+                    <button
+                      onClick={() => toggleAccordion(index)}
+                      className='flex justify-between w-full p-4 text-2xl'
+                    >
+                      {item.item_name}
+                      <MdOutlineKeyboardArrowRight
+                        size={30}
+                        className={`transition-transform duration-300 ${
+                          isAccordion === index
+                            ? 'rotate-90 md:rotate-180'
+                            : 'rotate-0'
+                        }`}
+                      />
+                    </button>
+                    {/* モバイル版 */}
                     <div className='border-b'></div>
-                    <li key={index}>
-                      <button
-                        onClick={() => toggleAccordion(index)}
-                        className='flex justify-between w-full p-4 text-2xl'
-                      >
-                        {item.item_name}
-                        <MdOutlineKeyboardArrowRight
-                          size={30}
-                          className={`transition-transform duration-300 ${
-                            isAccordion === index ? 'rotate-180' : 'rotate-0'
-                          }`}
-                        />
-                      </button>
-                      {/* モバイル版 */}
-                      <div className='border-b'></div>
-                      {isAccordion === index && (
-                        <div className='block md:hidden p-4'>
-                          <div className='w-96 border-2 p-1 mb-6'>
-                            <p className='mr-4 text-xl '>分類</p>
-                            <div className='text-3xl text-center'>
-                              {item.category_name}
-                            </div>
+                    {isAccordion === index && (
+                      <div className='block md:hidden p-4 bg-white'>
+                        <div className='w-96 p-1 mb-3'>
+                          <p className='mr-4 text-xl font-bold pb-2'>分類</p>
+                          <div className='text-lg ml-4'>
+                            {item.category_name}
                           </div>
-                          {item.item_memo !== null ? (
-                            <div className='w-96'>
-                              <p className='text-2xl font-normal pb-2'>出し方</p>
-                              {item.item_memo}
-                            </div>
-                          ) : null}
                         </div>
-                      )}
-                    </li>
-                  </>
+                        {item.item_memo !== null ? (
+                          <div className='max-w-full text-gray-600'>
+                            <p className='text-lg'>{item.item_memo}</p>
+                          </div>
+                        ) : (
+                          <div className='text-gray-600'>
+                            メモはありません
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </li>
                 ))
               )
             ) : (
@@ -163,80 +165,53 @@ const Item = () => {
                     <MdOutlineKeyboardArrowRight
                       size={30}
                       className={`transition-transform duration-300 ${
-                        isAccordion === index ? 'rotate-180' : 'rotate-0'
+                        isAccordion === index
+                          ? 'rotate-90 md:rotate-180'
+                          : 'rotate-0'
                       }`}
                     />
                   </button>
+                  {/* モバイル版 */}
                   <div className='border-b'></div>
                   {isAccordion === index && (
                     <div className='block md:hidden p-4 bg-white'>
-                      <div className='md:w-96 md:mb-6 mb-3'>
-                        <p className='font-bold text-xl pb-2'>分類</p>
-                        <div className='text-lg md:text-3xl pl-4'>
+                      <div className='w-96 p-1 mb-3'>
+                        <p className='mr-4 text-xl font-bold pb-2'>分類</p>
+                        <div className='text-lg ml-4'>
                           {item.category_name}
                         </div>
                       </div>
                       {item.item_memo !== null ? (
-                        <>
-                          <p className='text-xl pb-2 font-bold'>出し方</p>
-                          <div className='text-lg md:text-3xl pl-4'>
-                            {item.item_memo}
-                          </div>
-                        </>
-                      ) : null}
+                        <div className='max-w-full text-gray-600'>
+                          <p className='text-lg'>{item.item_memo}</p>
+                        </div>
+                      ) : (
+                        <div className='text-gray-600'>
+                          メモはありません
+                        </div>
+                      )}
                     </div>
                   )}
                 </li>
               ))
             )}
           </ul>
-        </div>
-        {/* PC版 */}
-        <div className='hidden md:w-3/5 md:block flex justify-center items-center'>
-          <div className='flex items-center justify-center right-panel h-full overflow-auto'>
-            {items.map((item, index) => (
-              isAccordion === index && (
-                <div className='w-112 bg-white rounded-lg p-4 mb-4' key={index}>
-                  <div className='p-4'>
-                    <p className='text-lg md:text-3xl font-normal pb-4 text-center'>{item.item_name}</p>
-                    <ul>
-                      <li className='text-lg'>
-                        <div className='w-96 border p-1 mb-6'>
-                          <p className='mr-4'>分類</p>
-                          <div className='text-3xl text-center'>
-                            {item.category_name}
-                          </div>
-                        </div>
-                        {item.item_memo !== null ? (
-                          <div className='w-96 border p-2'>
-                            <p className='text-2xl font-normal pb-2'>出し方</p>
-                            {item.item_memo}
-                          </div>
-                        ) : null}
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              )
-            ))}
+          {!showDetailSearch && (
+            <ArrowButton
+            nextPage={handleNextPage}
+            twoPagesAhead={handleTwoPagesAhead}
+            prevPage={handlePrevPage}
+            twoPagesBack={handleTwoPagesBack}
+            currentPage={pagination.current_page}
+            totalPages={pagination.last_page}
+          />
+          )}
           </div>
         </div>
-      </div>
-      <div className='md:w-2/5'>
-        <ArrowButton
-          nextPage={handleNextPage}
-          twoPagesAhead={handleTwoPagesAhead}
-          prevPage={handlePrevPage}
-          twoPagesBack={handleTwoPagesBack}
-          currentPage={pagination.current_page}
-          totalPages={pagination.last_page}
-        />
-      </div>
       <Footer />
     </>
   );
 };
 
 export default Item;
-
 
