bff8b49e596fab3f1303f6aea7666a90f2b06529
yumi <s_mog31@yahoo.co.jp>
2024-08-24 20:41:04 +0900

    clean:コメントの削除

--- gomi_navi_project/resources/js/components/Button/AddMemoButton.jsx
+++ gomi_navi_project/resources/js/components/Button/AddMemoButton.jsx
@@ -1,7 +1,64 @@
+// import React, { useEffect } from 'react';
+// import useModal from '../../hooks/useModal';
+// import MemoModal from '../Modal/MemoModal';
+// import { getMemos } from '../../api'; // APIからメモリストを取得する関数
+// import { useRecoilState } from 'recoil';
+// import { memoState } from '../../states/memoState';
+
+// const AddMemoButton = () => {
+//   const { isOpen, open, close } = useModal();
+//   const [memos, setMemos] = useRecoilState(memoState);
+
+//   useEffect(() => {
+//     // メモリストの変更を監視し、コンポーネントを更新する
+//     const fetchMemos = async () => {
+//       try {
+//         const response = await getMemos();
+//         setMemos(response.memos || []);
+//       } catch (error) {
+//         console.error(error.message);
+//       }
+//     };
+
+//     fetchMemos();
+//   }, []);
+
+//   const onSave = async (savedMemo) => {
+//     try {
+//       // メモの保存が成功した場合、メモリストを再取得して更新
+//       const response = await getMemos();
+//       setMemos(response.memos || []);
+//     } catch (error) {
+//       console.error('Error fetching memos:', error.message);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <button
+//         onClick={open}
+//         className='bg-white p-2 text-green-600  rounded-3xl hover:text-white hover:bg-green-500 transition-colors duration-300 text-sm sm:text-base '
+//       >
+//         メモを追加
+//         {isOpen &&
+//           <MemoModal
+//             isOpen={isOpen}
+//             onClose={close}
+//             onSave={onSave}
+//             title="新しいメモを作成"
+//             text="メモを作成"
+//           />}
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default AddMemoButton;
+
 import React, { useEffect } from 'react';
 import useModal from '../../hooks/useModal';
 import MemoModal from '../Modal/MemoModal';
-import { getMemos } from '../../api'; // APIからメモリストを取得する関数
+import { getMemos } from '../../api';
 import { useRecoilState } from 'recoil';
 import { memoState } from '../../states/memoState';
 
@@ -10,7 +67,6 @@ const AddMemoButton = () => {
   const [memos, setMemos] = useRecoilState(memoState);
 
   useEffect(() => {
-    // メモリストの変更を監視し、コンポーネントを更新する
     const fetchMemos = async () => {
       try {
         const response = await getMemos();
@@ -25,7 +81,6 @@ const AddMemoButton = () => {
 
   const onSave = async (savedMemo) => {
     try {
-      // メモの保存が成功した場合、メモリストを再取得して更新
       const response = await getMemos();
       setMemos(response.memos || []);
     } catch (error) {
@@ -40,18 +95,18 @@ const AddMemoButton = () => {
         className='bg-white p-2 text-green-600  rounded-3xl hover:text-white hover:bg-green-500 transition-colors duration-300 text-sm sm:text-base '
       >
         メモを追加
-        {isOpen &&
-          <MemoModal
-            isOpen={isOpen}
-            onClose={close}
-            onSave={onSave}
-            title="新しいメモを作成"
-            text="メモを作成"
-          />}
       </button>
+
+      {isOpen &&
+        <MemoModal
+          isOpen={isOpen}
+          onClose={close}
+          onSave={onSave}
+          title="新しいメモを作成"
+          text="メモを作成"
+        />}
     </div>
   );
 }
 
 export default AddMemoButton;
-
