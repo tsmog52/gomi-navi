0c4cf04f96dc49e6b9c752db9cd6450cbfe6f0c2
yumi <s_mog31@yahoo.co.jp>
2024-08-25 13:57:21 +0900

    style:レスポンシブ対応を追加

--- gomi_navi_project/resources/js/components/Modal/MemoDetailModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/MemoDetailModal.jsx
@@ -6,20 +6,21 @@ const MemoDetailModal = ({ memo, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-4 rounded shadow-lg max-w-md w-full">
+      <div className="bg-white p-4 md:p-8 rounded-lg shadow-lg max-w-xs md:max-w-lg w-full">
         <div className='block text-black flex justify-end'>
           <AiOutlineClose
             onClick={onClose}
             size={20}
+            className="cursor-pointer"
           />
         </div>
-        <p className="text-xl font-bold mb-2">タイトル</p>
+        <p className="text-lg md:text-xl font-bold mb-2">タイトル</p>
         <p
           className='text-gray-600 font-bold block flex justify-start ml-4 bg-gray-100 p-2 border-gray-100 rounded-lg'
         >
           {memo.title}
         </p>
-        <p className="text-xl font-bold m-2 ">内容</p>
+        <p className="text-lg md:text-xl font-bold mt-4 mb-2">内容</p>
         <p
           className="text-gray-600 font-bold block flex justify-start ml-4 bg-gray-100 border-gray-100 rounded-lg p-2 mb-2"
         >
