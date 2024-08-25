d23e209aa1051a7e4ccd3e053f2c12c27574865a
yumi <s_mog31@yahoo.co.jp>
2024-08-25 12:07:44 +0900

    feat:モーダルの開閉機能を追加

--- gomi_navi_project/resources/js/components/MemoList.jsx
+++ gomi_navi_project/resources/js/components/MemoList.jsx
@@ -45,6 +45,11 @@ import { memoState } from '../states/memoState';
     setSelectedMemo(null);
   }
 
+  const handleMemoClick = (memo) => {
+    setSelectedMemo(memo);
+    setShowDetailModal(true);
+  };
+
   return (
     <>
       <div className='flex flex-col items-center p-4'>
