f86c04715ffeb336dee8aceda5a23dca8685c0b1
yumi <s_mog31@yahoo.co.jp>
2024-08-25 12:23:55 +0900

    style:iconの追加、テキストカラーの変更

--- gomi_navi_project/resources/js/components/Modal/MemoDetailModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/MemoDetailModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiOutlineClose } from "react-icons/ai";
 
 const MemoDetailModal = ({ memo, onClose }) => {
   if (!memo) return null;
@@ -6,14 +7,16 @@ const MemoDetailModal = ({ memo, onClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-4 rounded shadow-lg max-w-md w-full">
-        <h2 className="text-xl font-bold mb-2">{memo.title}</h2>
-        <p className="text-lg mb-4">{memo.note}</p>
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-          onClick={onClose}
-        >
-          閉じる
-        </button>
+        <div className='block text-black flex justify-end'>
+          <AiOutlineClose
+            onClick={onClose}
+            size={20}
+          />
+        </div>
+        <p className="text-xl font-bold mb-2">タイトル</p>
+        <p className='text-gray-600 font-bold block flex justify-start ml-4 bg-gray-200 p-2'>{memo.title}</p>
+        <p className="text-xl font-bold mb-2">内容</p>
+        <p className="text-gray-600 font-bold block flex justify-start ml-4 bg-gray-200 p-2">{memo.note}</p>
       </div>
     </div>
   );
