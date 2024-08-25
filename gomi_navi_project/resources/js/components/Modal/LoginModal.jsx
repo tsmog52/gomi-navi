076d4f990764eb716aaa5df7e2e0ad572669fc9f
yumi <s_mog31@yahoo.co.jp>
2024-08-25 12:35:36 +0900

    feat:LineButtonにdisabledを追加

--- gomi_navi_project/resources/js/components/Modal/LoginModal.jsx
+++ gomi_navi_project/resources/js/components/Modal/LoginModal.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import CloseButton from '../Button/CloseButton';
 import LineButton from '../Button/LineButton';
+import { useRecoilValue } from 'recoil';
+import { loginState } from '../states/loginState';
 
 const LoginModal = ({ title, onClose, google, line }) => {
+  const user = useRecoilValue(loginState);
+  const isLoggedIn = !!user;
   return (
     <>
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4 md:p-4">
@@ -15,7 +19,11 @@ const LoginModal = ({ title, onClose, google, line }) => {
             <p className='text-sm md:text-base pb-2'>
               LINEアカウントでリマインダー通知を<br />受け取ることができます。
             </p>
-            <LineButton text={line} socialLink={'/auth/line'} />
+            <LineButton
+              text={line}
+              socialLink={'/auth/line'}
+              disabled={isLoggedIn}
+            />
           </div>
         </div>
       </div>
