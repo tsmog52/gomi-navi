6dc35fc01b22b8a85c329825cfdd1edf4321eb6c
yumi <s_mog31@yahoo.co.jp>
2024-08-25 14:53:57 +0900

    feat:キャンセルボタンの追加

--- gomi_navi_project/resources/js/components/MemoList.jsx
+++ gomi_navi_project/resources/js/components/MemoList.jsx
@@ -96,6 +96,12 @@ import { memoState } from '../states/memoState';
                       >
                         削除
                       </button>
+                      <button
+                        className="w-full px-4 py-2 text-left text-gray-600 hover:bg-gray-100"
+                        onClick={closeEditDeleteModal}
+                      >
+                        戻る
+                      </button>
                     </div>
                   )}
                 </div>
