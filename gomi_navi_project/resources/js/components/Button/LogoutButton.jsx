0e9b7c329d22629d0fd76c05c903b340b7b1d993
yumi <s_mog31@yahoo.co.jp>
2024-08-30 10:04:28 +0900

    feat:リダイレクト処理の追加

--- gomi_navi_project/resources/js/components/Button/LogoutButton.jsx
+++ gomi_navi_project/resources/js/components/Button/LogoutButton.jsx
@@ -1,61 +1,3 @@
-// import React from 'react';
-// import axios from 'axios';
-// import Cookies from 'js-cookie';
-// import { useNavigate } from 'react-router-dom';
-
-// const LogoutButton = () => {
-//   const navigate = useNavigate();
-
-//   const handleLogout = async () => {
-//     try {
-//       // メタタグからCSRFトークンを取得
-//       const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-//       axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
-
-//       // クッキーを削除
-//       Cookies.remove('auth_token');
-//       Cookies.remove('user_id');
-
-//       // API経由でログアウト処理を行う
-//       await axios.post('/api/logout', {}, {
-//         withCredentials: true
-//       });
-
-//       // ログアウト後のリダイレクト
-//       window.location.href = '/'
-//     } catch (error) {
-//       // エラーログの詳細な出力
-//       if (error.response) {
-//         // サーバーがステータスコードを返した場合
-//         console.error('Error logging out:', {
-//           status: error.response.status,
-//           data: error.response.data,
-//           headers: error.response.headers
-//         });
-//       } else if (error.request) {
-//         // リクエストが送信されたが、応答がなかった場合
-//         console.error('Error logging out: No response received', {
-//           request: error.request
-//         });
-//       } else {
-//         // リクエストの設定で問題があった場合
-//         console.error('Error logging out: Request setup issue', {
-//           message: error.message
-//         });
-//       }
-//     }
-//   };
-
-//   return (
-//     <button
-//       onClick={handleLogout}
-//       className='bg-white p-2 text-green-600  rounded-3xl hover:text-white hover:bg-green-500 transition-colors duration-300 text-sm sm:text-base '
-//     >ログアウト</button>
-//   );
-// };
-
-// export default LogoutButton;
-
 import React from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
@@ -75,12 +17,9 @@ const LogoutButton = () => {
         withCredentials: true
       });
 
-      // ログアウト後のリダイレクト
-      navigate('/');
+      window.location.href = '/';
     } catch (error) {
       console.error('Error logging out:', error);
-
-      // ユーザーにエラーメッセージを表示する処理を追加すると良いでしょう
     }
   };
 
